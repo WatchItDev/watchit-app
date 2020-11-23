281c5d1fc366233d8891cd861d0b65f40a26b508
Geolffrey Mena <gmjun2000@gmail.com>
2020-11-23 10:01:32 -0600

    ENV settings for listen address

--- public/lib/settings/orbit.js
+++ public/lib/settings/orbit.js
@@ -1,4 +1,9 @@
 module.exports = {
     BOOTSTRAP_IP: '127.0.0.1',
-    BOOTSTRAP_HASH: 'QmYye19FqWHncUTqzAHs9uQBeJmCQeVAJdekWwzihy8Su7'
+    BOOTSTRAP_HASH: 'QmYye19FqWHncUTqzAHs9uQBeJmCQeVAJdekWwzihy8Su7',
+    BOOTSTRAP_LISTEN: [
+        '/dns4/secure-beyond-12878.herokuapp.com/tcp/443/wss/p2p-webrtc-star/',
+        '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+        '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/'
+    ]
 }
