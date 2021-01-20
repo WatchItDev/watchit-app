de493321a613b93e46fc4ab4e5c19e003c2e5b46
Geolffrey Mena <gmjun2000@gmail.com>
2021-01-19 23:41:27 -0600

    Fancy object XD

--- public/lib/settings/conf.js
+++ public/lib/settings/conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const fs = require('fs')
 
 const ROOT_DIR = os.tmpdir();
+const READ_TIMEOUT_FILE = 500
 const ROOT_URI = 'http://ipfs.io/ipfs/';
 const ROOT_URI_TORRENT = `${ROOT_URI}`;
 const ROOT_STORE = process.env.appPath;
@@ -12,13 +13,13 @@ const ROOT_PUBLIC = path.join(ROOT_APP, 'public');
 const ROOT_DB_FILE = path.join(ROOT_STORE, 'w_alloc', 'wt.db')
 
 module.exports = {
-    'READ_TIMEOUT_FILE': 500,
-    'ROOT_APP': ROOT_APP,
-    'ROOT_URI': ROOT_URI,
-    'ROOT_DIR': ROOT_DIR,
-    'ROOT_STORE': ROOT_STORE,
-    'ROOT_PUBLIC': ROOT_PUBLIC,
-    'ROOT_DB_FILE': ROOT_DB_FILE,
-    'ROOT_TMP_FOLDER': ROOT_TMP_FOLDER,
-    'ROOT_URI_TORRENT': ROOT_URI_TORRENT
+    ROOT_APP,
+    ROOT_URI,
+    ROOT_DIR,
+    ROOT_STORE,
+    ROOT_PUBLIC,
+    ROOT_DB_FILE,
+    ROOT_TMP_FOLDER,
+    READ_TIMEOUT_FILE,
+    ROOT_URI_TORRENT
 };
