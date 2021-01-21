b4ef4cb3937e8de45f8e9e60188ebe95960ddb6f
Geolffrey Mena <gmjun2000@gmail.com>
2021-01-21 15:37:26 -0600

    Fix ensure dir ipfs before init

--- public/lib/ipfs.js
+++ public/lib/ipfs.js
@@ -1,12 +1,12 @@
+const fs = require('fs-extra');
 const Ctl = require('ipfsd-ctl')
 const ipfsConf = require('./settings/ipfs');
-const {ROOT_RUNNING_TIME} = require('./settings/conf')
-const fs = require('fs-extra');
+const {ROOT_RUNNING_TIME, ROOT_IPFS_DIR} = require('./settings/conf')
 
 
 module.exports = async () => {
     const isInstance = await Ctl.createController({
-        ipfsOptions: {config: ipfsConf(), repo: ROOT_RUNNING_TIME},
+        ipfsOptions: {config: ipfsConf(), repo: ROOT_IPFS_DIR},
         ipfsHttpModule: require('ipfs-http-client'),
         ipfsBin: require('go-ipfs').path(),
         disposable: false, forceKillTimeout: 2000,
