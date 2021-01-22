4871141456a3a1c2848b389b788966157a34cc0b
Geolffrey Mena <gmjun2000@gmail.com>
2021-01-21 18:35:51 -0600

    Fix Sandbox

--- scripts/entrypoint.js
+++ scripts/entrypoint.js
@@ -1,16 +1,22 @@
 const os = require('os');
-const isWin = os.type() === 'Windows_NT'
-const fs = require('fs')
+const osType = os.type().toLowerCase()
+const isWin = osType === 'windows_nt' || osType === 'win32'
+const isLinux = osType === 'linux'
 const {execPassthru} = require('./util')
 
+console.log(osType);
 if (isWin) execPassthru('npm i windows-elevate')
 const {exec} = require(isWin ? 'windows-elevate' : 'child_process')
 
+if (isLinux) {
+    // Fix The SUID sandbox helper binary was found
+    execPassthru('sudo chown root.root node_modules/electron/dist/chrome-sandbox -R', exec)
+    execPassthru('sudo chmod 4755 -R node_modules/electron/dist/chrome-sandbox', exec)
+}
 
+execPassthru('cd node_modules/unzip/node_modules/fstream/ && npm i graceful-fs@4.2.4 --save', exec)
 execPassthru('npm install go-ipfs@0.6.0', exec)
 execPassthru('electron-builder install-app-deps', exec)
 execPassthru('npm rebuild ursa-optional', exec)
-execPassthru('cd node_modules/unzip/node_modules/fstream/ && npm i graceful-fs@4.2.4 --save', exec)
-
 
 
