706b7fbd5c237d41f3fe116fa92fbcd2efccbcb6
geolffrey <gmjun2000@gmail.com>
2021-01-24 12:24:12 -0600

    Checking pre commit fail

--- src/js/front/components/partials/app-image/index.test.jsx
+++ src/js/front/components/partials/app-image/index.test.jsx
@@ -2,32 +2,37 @@ import React from "react";
 import {render, unmountComponentAtNode} from "react-dom";
 import {act} from "react-dom/test-utils";
 import BoxImage from "./index";
+import pretty from "pretty";
 
 let container = null;
 beforeEach(() => {
-    window.env = {ROOT_URI: '/test/'}
-    container = document.createElement('div')
-    document.body.appendChild(container)
-})
+    window.env = {ROOT_URI: "/test/"};
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
 
 afterEach(() => {
-    unmountComponentAtNode(container)
-    container.remove()
-    container = null
-})
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
 
 describe("Image component", () => {
-
-    it('Render image box with valid uri', () => {
+    it("Should render image box", () => {
         act(() => {
-            render(<BoxImage src={'large_cover_image.jpg'}/>, container)
-        })
+            render(<BoxImage src={"large_cover_image.jpg"}/>, container);
+        });
 
-        expect(
-            container.querySelector('img').src
-        ).toBe('http://localhost/test/large_cover_image.jpg')
+        expect(pretty(container.innerHTML)).toMatchSnapshot();
+    });
 
-    })
+    it("Should render image box with valid uri", () => {
+        act(() => {
+            render(<BoxImage src={"large_cover_image.jpg"}/>, container);
+        });
 
-
-})
+        expect(container.querySelector("img").src).toBe(
+            "http://localhost/test/large_cover_image.jg"
+        );
+    });
+});
