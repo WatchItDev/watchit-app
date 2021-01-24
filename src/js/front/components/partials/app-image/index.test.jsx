416e3e65d976ab928dd8af5d5fcac28d8a0722da
geolffrey <gmjun2000@gmail.com>
2021-01-24 01:44:25 -0600

    Fix exec entrypoint.js

--- src/js/front/components/partials/app-image/index.test.jsx
+++ src/js/front/components/partials/app-image/index.test.jsx
@@ -16,11 +16,18 @@ afterEach(() => {
     container = null
 })
 
-it('Render image box with valid uri', () => {
-    act(() => {
-        render(<BoxImage src={'large_cover_image.jpg'}/>, container)
+describe("Image component", () => {
+
+    it('Render image box with valid uri', () => {
+        act(() => {
+            render(<BoxImage src={'large_cover_image.jpg'}/>, container)
+        })
+
+        expect(
+            container.querySelector('img').src
+        ).toBe('http://localhost/test/large_cover_image.jpg')
+
     })
 
-    expect(container.querySelector('img').src).toBe('/test/large_cover_image.jpg')
 
 })
