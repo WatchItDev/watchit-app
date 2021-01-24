171bd5e48ccddce5e0709a34a8ab19377927b52f
geolffrey <gmjun2000@gmail.com>
2021-01-24 17:05:56 -0600

    Fix go-ipfs bin directory in prod environment

--- public/lib/ipfs.js
+++ public/lib/ipfs.js
@@ -1,12 +1,34 @@
+const fs = require('fs')
+const path = require('path')
 const Ctl = require('ipfsd-ctl')
 const ipfsConf = require('./settings/ipfs');
 
+const inDev = Object.is(process.env.ENV, 'dev')
+const resolveIpfsPaths = () => {
+    /***
+     * Check for relative path for go-ipfs in prod env
+     * @type {string}
+     */
+    if (inDev) return require('go-ipfs').path()
+    const runtimeDir = path.dirname(path.resolve(__dirname, '..'))
+    const rootUnpacked = path.join(`${runtimeDir}.unpacked`, 'node_modules', 'go-ipfs')
+    const paths = [
+        path.join(rootUnpacked, 'go-ipfs', 'ipfs'),
+        path.join(rootUnpacked, 'go-ipfs', 'ipfs.exe'),
+    ]
+
+    for (const bin of paths) {
+        if (fs.existsSync(bin)) {
+            return bin
+        }
+    }
+}
 
 module.exports = async () => {
     const isInstance = await Ctl.createController({
         ipfsOptions: {config: ipfsConf()},
         ipfsHttpModule: require('ipfs-http-client'),
-        ipfsBin: require('go-ipfs').path(),
+        ipfsBin: resolveIpfsPaths(),
         disposable: false, forceKillTimeout: 2000,
         args: ['--enable-pubsub-experiment'],
         remote: false, type: 'go'
