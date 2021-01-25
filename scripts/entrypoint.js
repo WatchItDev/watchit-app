3a812687c42bc25171f353457de9c1ddc0714c5a
geolffrey <gmjun2000@gmail.com>
2021-01-24 20:10:13 -0600

    Fix go-ipfs dep windows crash

--- scripts/entrypoint.js
+++ scripts/entrypoint.js
@@ -12,13 +12,6 @@ const executePostInstall = async () => {
         await execPassthru('sudo chmod 4755 -R node_modules/electron/dist/chrome-sandbox')
     }
 
-    // Installing fallback OS
-    log.info('Installing ipfs in', osType)
-    try {
-        await execPassthru('npm install go-ipfs@0.6.0 --no-save', await getElevateExec())
-    } catch (err) {
-        log.error(err)
-    }
 
     await execPassthru('cd node_modules/unzip/node_modules/fstream/ && npm i graceful-fs@4.2.4 --no-save')
     await execPassthru('electron-builder install-app-deps')
