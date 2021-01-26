60c9bf9259f56a7cdf007fcfd577d081d1954b98
geolffrey <gmjun2000@gmail.com>
2021-01-25 19:31:27 -0600

    Add some docs to preload.js -- fix typo

--- public/preload.js
+++ public/preload.js
@@ -1,5 +1,5 @@
 /***
- * his script will always have access to node APIs no matter whether node integration is turned on or off.
+ * This script will always have access to node APIs no matter whether node integration is turned on or off.
  * The value should be the absolute file path to the script. When node integration is turned off, the preload script can
  * reintroduce Node global symbols back to the global scope.
  * https://www.electronjs.org/docs/api/browser-window
