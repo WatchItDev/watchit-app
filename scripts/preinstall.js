2c8173c0059eb63d1841c90d4eb6a97595f5da7d
Geolffrey Mena <gmjun2000@gmail.com>
2021-01-26 01:02:29 -0600

    Update preinstall.js

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -2,6 +2,7 @@ const {execPassthru, getElevateExec} = require('./util');
 
 const executePreInstall = async () => {
     try {
+        await execPassthru('npm install electron-log --no-save')
         await execPassthru('npm install go-ipfs@0.6.0 --no-save', await getElevateExec());
     } catch (err) {
         log.error(err);
