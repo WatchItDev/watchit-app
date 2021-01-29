0ec8dcb5014243b59de65663627143695495c893
geolffrey <gmjun2000@gmail.com>
2021-01-29 16:16:42 -0600

    Fix gateway

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -4,7 +4,7 @@ const {execPassthru, getElevateExec,  osType} = require('./util');
 const executePreInstall = async () => {
     log.info('Installing ipfs in', osType);
     try {
-        await execPassthru('npm install go-ipfs@0.7.0 --no-save', await getElevateExec());
+        await execPassthru('npm install go-ipfs@0.6.0 --no-save', await getElevateExec());
     } catch (err) {
         log.error(err);
     }
