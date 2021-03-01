ef300a35996acb67171323122ccbf4a9720b82e8
geolffrey <gmjun2000@gmail.com>
2021-03-01 14:12:53 -0600

    fix malformed initialization for image resource

--- src/core/app/components/app-movies-top-search-result/index.jsx
+++ src/core/app/components/app-movies-top-search-result/index.jsx
@@ -35,7 +35,7 @@ export default class AppMainSearchResult extends React.Component {
                                             this.props.result.map((i) => {
                                                 return (
                                                     <AppMainResultBox
-                                                        key={i._id} {...Object.assign(i, {image: `${i.small_image}`})}
+                                                        key={i._id} {...Object.assign(i, {image: i.small_image})}
                                                         onClick={this.props.onClick}
                                                     />
                                                 )
