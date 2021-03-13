64478caeb4dd1efca833c0eeacb44c18472caa0e
jadapema <Ruperma040193>
2021-03-12 23:47:30 -0600

    feat: add cancel button once login

--- src/core/app/components/app-movie-player-loader/index.jsx
+++ src/core/app/components/app-movie-player-loader/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import AppLoaderBackground from 'components/app-movie-player-loader-background/'
 import AppLoaderBar from 'components/app-movie-player-loader-bar/'
+import AppBtnClose from 'components/util-btn-close/'
 
 export default class AppMoviesPlayerLoader extends React.PureComponent {
 	static get propTypes() {
@@ -15,9 +16,7 @@ export default class AppMoviesPlayerLoader extends React.PureComponent {
 		return (
 			<div className="output-process valign-wrapper full-width full-height">
 				<div className="app_loader">
-					<a href="#" className="btn-close clearfix font-size-45" onClick={this.props.signOut}>
-						<i className="icon-cross white-text" />
-					</a>
+					<AppBtnClose onClick={this.props.signOut} />
 					<AppLoaderBackground/>
 					<AppLoaderBar
 						stateText={this.props.stateText}
