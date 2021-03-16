f560eb7cd1a2bfdfce227cc895495c4f6eebe850
geolffrey <gmjun2000@gmail.com>
2021-03-16 16:01:52 -0600

    fix: invalid state

--- src/core/app/pages/index-view/index.jsx
+++ src/core/app/pages/index-view/index.jsx
@@ -25,7 +25,7 @@ export default class MovieIndex extends React.Component {
         //Default state
         this.state = {
             state: 'Initializing', percent: 0, peers: this.peers, count: DEFAULT_INIT_LOAD,
-            ready: false, loading: true, movies: [], chunkSize: setting.defaults.chunkSize,
+            ready: false, loading: true, movies: [], chunkSize: util.calcScreenSize(),
             lock: false, // Avoid re-render movies list
             finishLoad: false, showDetailsFor: false, logout: false
         };
