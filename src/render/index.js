bcd3af4c6263fff3476b6617125aadb0b802e307
geolffrey <gmjun2000@gmail.com>
2021-03-23 14:38:19 -0600

    chore: add normalize.css dep

--- src/render/index.js
+++ src/render/index.js
@@ -5,47 +5,43 @@ import MovieIndex from 'pages/index-view';
 import MoviePlayer from 'pages/player-view';
 import DragBar from 'components/app-drag-bar/index'
 
-import * as serviceWorker from './serviceWorker';
-import {createBrowserHistory} from 'history';
-import {HashRouter, Switch, Route, Redirect} from "react-router-dom";
+import 'normalize.css'
 import './index.sass';
 import 'plyr/dist/plyr.css';
 
+import {createBrowserHistory} from 'history';
+import {HashRouter, Switch, Route, Redirect} from "react-router-dom";
+
 const key = window.bridge.Key
 const hist = createBrowserHistory({
-	basename: "/", // The base URL of the app (see below)
-	forceRefresh: false, // Set true to force full page refreshes
-	hashType: 'slash'
+    basename: "/", // The base URL of the app (see below)
+    forceRefresh: false, // Set true to force full page refreshes
+    hashType: 'slash'
 });
 
 
 //Require logged
 function DragBarHOC(Component, navigate) {
-	return <DragBar>
-		<Component {...navigate}/>
-	</DragBar>
+    return <DragBar>
+        <Component {...navigate}/>
+    </DragBar>
 }
 
 ReactDOM.render(
-	<HashRouter history={hist}>
-		<Switch>
-			<Route name="login" exact path="/"
-				   render={(n) => !key.isLogged()
-					   ? DragBarHOC(Login, n) : (<Redirect to="/app/movies"/>)}/>
-			<Route name="movies" exact path="/app/movies"
-				   render={(n) => (DragBarHOC(MovieIndex, n))}/>
-			<Route name="player"
-				   path="/play/:resource"
-				   render={(n) => (DragBarHOC(MoviePlayer, n))}/>
-		</Switch>
-	</HashRouter>,
-	document.getElementById('root')
+    <HashRouter history={hist}>
+        <Switch>
+            <Route name="login" exact path="/"
+                   render={(n) => !key.isLogged()
+                       ? DragBarHOC(Login, n) : (<Redirect to="/app/movies"/>)}/>
+            <Route name="movies" exact path="/app/movies"
+                   render={(n) => (DragBarHOC(MovieIndex, n))}/>
+            <Route name="player"
+                   path="/play/:resource"
+                   render={(n) => (DragBarHOC(MoviePlayer, n))}/>
+        </Switch>
+    </HashRouter>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
 //LISTENERS
 const preventDefault = (e) => e.preventDefault()
@@ -58,12 +54,12 @@ window.addEventListener("dragstart", preventDefault, false);
 document.addEventListener('contextmenu', preventDefault, false);
 //Prevent default reload, devtools
 document.addEventListener('keydown', (e) => {
-	let keyCode = !window.event ? (e.which || e.keyCode) : window.event.keyCode;
-	let avoidReload = (e.ctrlKey && keyCode === 82 && process.env.NODE_ENV !== 'development')
-	// this code handles the F5/Ctrl+F5/Ctrl+R
-	if (keyCode === 116 || avoidReload)
-		e.preventDefault();
-	if (keyCode === 122 || keyCode === 123)
-		e.preventDefault()
-	
+    let keyCode = !window.event ? (e.which || e.keyCode) : window.event.keyCode;
+    let avoidReload = (e.ctrlKey && keyCode === 82 && process.env.NODE_ENV !== 'development')
+    // this code handles the F5/Ctrl+F5/Ctrl+R
+    if (keyCode === 116 || avoidReload)
+        e.preventDefault();
+    if (keyCode === 122 || keyCode === 123)
+        e.preventDefault()
+
 });
