cb8098e4f82423faf9090566290bf1a0212c6207
geolffrey <gmjun2000@gmail.com>
2021-03-23 17:15:33 -0600

    feat: add db browser support

--- src/main/core/key/browser/index.js
+++ src/main/core/key/browser/index.js
@@ -1,7 +1,6 @@
-const orbit = require('orbit-db')
+const Key = require('../key')
 
-
-module.exports = class Key {
+module.exports = class BrowserKey extends Key {
 
     static get db() {
         return 'w_db'
@@ -19,57 +18,6 @@ module.exports = class Key {
         return Key.isLogged()
     }
 
-
-    static generateKey(data) {
-        return this.addToStorage(data)
-    }
-
-    static isValidKey(key) {
-        return orbit.isValidAddress(
-            this.sanitizedKey(key)
-        )
-    }
-
-    static sanitizedKey(key) {
-        return `/orbitdb/${key}/wt.movies.db`
-    }
-
-    static isLogged() {
-        return !!Key.read();
-    }
-
-    static readFromStorage() {
-        try {
-            return JSON.parse(
-                Key.read()
-            );
-        } catch (e) {
-            console.log('Invalid JSON');
-            return {}
-        }
-
-    }
-
-    static removeFromStorage(index) {
-        let currentData = Key.readFromStorage()
-        if (currentData && index in currentData) {
-            delete currentData[index]
-            Key.write(currentData)
-        }
-    }
-
-    static addToStorage(data = {}) {
-        let currentData = Key.readFromStorage()
-        let extendedData = Object.assign({}, currentData, data)
-        Key.write(extendedData)
-    }
-
-    static getIngestKey() {
-        let fileCollection = Key.readFromStorage()
-        return fileCollection && 'ingest' in fileCollection
-            ? fileCollection.ingest.trim() : null
-
-    }
 
     static write(data) {
         localStorage.setItem('key', JSON.stringify(data))
--- src/main/core/key/node/index.js
+++ src/main/core/key/node/index.js
@@ -1,92 +1,41 @@
 const fs = require('fs-extra');
 const path = require('path');
-const orbit = require('orbit-db')
+const Key = require('../key')
 const {
     ROOT_DB_DIR,
     ROOT_STORE
 } = require('../../settings')
 
-module.exports = class Key {
-
-    static get engine() {
-        return 'leveldown'
-    }
+module.exports = class NodeKey extends Key {
 
     static get db() {
         return ROOT_DB_DIR
     }
 
+    static get engine() {
+        return 'leveldown'
+    }
+
     static get init() {
-        fs.ensureDirSync(this.db)
+        fs.ensureDirSync(NodeKey.db)
         return this
     }
 
     static get existKey() {
-        return fs.existsSync(Key.keyFile)
+        return fs.existsSync(NodeKey.keyFile)
     }
 
     static get keyFile() {
         return path.join(ROOT_STORE, 'key.json')
     }
 
-    static generateKey(data) {
-        return this.addToStorage(data)
-    }
-
-    static isValidKey(key) {
-        return orbit.isValidAddress(
-            this.sanitizedKey(key)
-        )
-    }
-
-    static sanitizedKey(key) {
-        return `/orbitdb/${key}/wt.movies.db`
-    }
-
-    static isLogged() {
-        return !!Key.read();
-    }
-
-    static readFromStorage() {
-        try {
-            return JSON.parse(
-                Key.read()
-            );
-        } catch (e) {
-            console.log('Invalid JSON');
-            return {}
-        }
-
-    }
-
-    static removeFromStorage(index) {
-        let currentData = Key.readFromStorage()
-        if (currentData && index in currentData) {
-            delete currentData[index]
-            Key.write(currentData)
-        }
-    }
-
-    static addToStorage(data = {}) {
-        let currentData = Key.readFromStorage()
-        let extendedData = Object.assign({}, currentData, data)
-        Key.write(extendedData)
-    }
-
-    static getIngestKey() {
-        let fileCollection = Key.readFromStorage()
-        return fileCollection && 'ingest' in fileCollection
-            ? fileCollection.ingest.trim() : null
-
-    }
-
-    static write(data, file = Key.keyFile) {
+    static write(data, file = NodeKey.keyFile) {
         fs.writeFileSync(file, JSON.stringify(data));
     }
 
     static read() {
         return this.existKey ?
-            fs.readFileSync(Key.keyFile) : null
+            fs.readFileSync(NodeKey.keyFile) : null
     }
 };
 
