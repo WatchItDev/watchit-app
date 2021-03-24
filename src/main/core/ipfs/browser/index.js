0aab75b322fa0bab9686c2d2f6a849ef9865f7a9
geolffrey <gmjun2000@gmail.com>
2021-03-24 12:13:59 -0600

    fix: paths and params for browser

--- src/main/core/ipfs/browser/index.js
+++ src/main/core/ipfs/browser/index.js
@@ -1,10 +1,10 @@
-const fs = require('fs')
 const log = require('electron-log')
 const IPFS = require('ipfs');
-const ipfsConf = require('./p2p');
+const libp2p = require('./p2p');
+const defaultConf = require('../settings')
 
 export default async (repo) => {
-    const CONF = Object.assign({libp2p}, {repo}, ipfsConf);
+    const CONF = Object.assign({libp2p}, {repo}, defaultConf());
     const isInstance = await IPFS.create(CONF)
     log.info(`Started ${isInstance.started}`)
     log.info('Running ipfs id', isInstance.id())
