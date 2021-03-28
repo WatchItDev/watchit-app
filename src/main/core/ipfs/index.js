e2555ea8281156b41e5b850a70c5df46b4d22ec0
geolffrey <gmjun2000@gmail.com>
2021-03-27 19:39:39 -0600

    docs: node types and capabilities

--- src/main/core/ipfs/index.js
+++ src/main/core/ipfs/index.js
@@ -1,6 +1,6 @@
 const log = require('electron-log')
 const isBrowser = typeof process === 'undefined'
-log.warn(`Running ${isBrowser ? 'Browser' : 'Node'} env`)
+log.warn(`Running IPFS ${isBrowser ? 'Browser' : 'Node'} env`)
 
 module.exports = require(
     // Handle multiple envs for browser or node package
