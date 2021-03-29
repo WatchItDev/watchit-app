90ccf18cbed05f2dc05b3c5c49fea751e4ffe3f3
geolffrey <gmjun2000@gmail.com>
2021-03-28 21:58:39 -0600

    chore: add node-prune

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -5,7 +5,6 @@ const executePreInstall = async () => {
     log.info('Installing ipfs in', osType);
     try {
         await execPassthru('npm install go-ipfs@0.6.0 --no-save', await getElevateExec());
-        await execPassthru('curl -sf https://gobinaries.com/tj/node-prune | bash', await getElevateExec());
     } catch (err) {
         log.error(err);
     }
