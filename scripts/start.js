92c88f884d8600bc6c36561b4ddb7896b60f4f71
geolffrey <gmjun2000@gmail.com>
2021-03-29 12:55:11 -0600

    chore: bump react-script

--- scripts/start.js
+++ scripts/start.js
@@ -160,7 +160,7 @@ checkBrowsers(paths.appPath, isInteractive)
   })
   .catch(err => {
     if (err && err.message) {
-      console.log(err.message);
+      // console.log(err.message);
     }
     process.exit(1);
   });
