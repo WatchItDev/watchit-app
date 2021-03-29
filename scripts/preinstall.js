b7db8b74dc33117990b67464e916912de0ea95c7
geolffrey <gmjun2000@gmail.com>
2021-03-29 11:35:20 -0600

    chore: node-prune.sh

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,10 +1,11 @@
 const log = require('electron-log')
-const {execPassthru, getElevateExec,  osType} = require('./util');
+const {execPassthru, getElevateExec, osType} = require('./util');
 
 const executePreInstall = async () => {
     log.info('Installing ipfs in', osType);
     try {
         await execPassthru('npm install go-ipfs@0.6.0 --no-save', await getElevateExec());
+        await execPassthru('bash -c "source scripts/node-prune.sh"', await getElevateExec());
     } catch (err) {
         log.error(err);
     }
