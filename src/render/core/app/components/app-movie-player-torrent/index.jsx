a2d8bac4cff2d14391a109f58b1df4ee841bb0e9
geolffrey <gmjun2000@gmail.com>
2021-03-30 09:47:43 -0600

    fix: avoid try render from torrent progress after component did unmount

--- src/render/core/app/components/app-movie-player-torrent/index.jsx
+++ src/render/core/app/components/app-movie-player-torrent/index.jsx
@@ -49,7 +49,7 @@ export default class AppMoviesPlayerTorrent extends React.PureComponent {
             <>
                 {
                     (
-                        !this.state.canPlay && !this.state.flix &&
+                        !this.state.canPlay &&
                         <div className="absolute full-width full-height player-overlay-loader">
                             <AppMoviePlayerLoader
                                 stateText={this.state.state}
