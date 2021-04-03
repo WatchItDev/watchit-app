a0646fad76468e179ac3dc7e5a65c6c0f6a35545
geolffrey <gmjun2000@gmail.com>
2021-04-03 16:36:20 -0600

    chore: remove dead code

--- src/render/index.js
+++ src/render/index.js
@@ -6,19 +6,16 @@ import 'normalize.css'
 import './index.sass';
 import 'plyr/dist/plyr.css';
 
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 const root = document.getElementById('root')
-if (root.hasChildNodes()) {
-    ReactDOM.hydrate(<App/>, root);
-} else {
-    ReactDOM.render(<App/>, root);
-}
+ReactDOM.render(<App/>, root);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
 
 //LISTENERS
 const preventDefault = (e) => e.preventDefault()
