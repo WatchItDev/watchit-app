7e3f0a1583d39b5fe2debd3e3d892403ad5f85bc
geolffrey <gmjun2000@gmail.com>
2021-04-12 15:58:58 -0600

    style: fix lint warning

--- src/main/core/dlna/index.js
+++ src/main/core/dlna/index.js
@@ -1,5 +1,5 @@
 module.exports = require(
     // Handle multiple envs for browser or node package
-    typeof process === 'undefined' && './browser' || './node'
+    typeof process === 'undefined' ? './browser' : './node'
 )
 
--- src/main/core/torrent/index.js
+++ src/main/core/torrent/index.js
@@ -1,6 +1,5 @@
-const runtime = process.env.RUNTIME // web | node | electron
 module.exports = require(
     // Handle multiple envs for browser or node package
-    typeof process === 'undefined' && './browser' || './node'
+    typeof process === 'undefined' ? './browser' : './node'
 )
 
