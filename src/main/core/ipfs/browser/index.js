666978de34dbe9c7020aaea3f93933e0cd32f9f5
geolffrey <gmjun2000@gmail.com>
2021-04-16 16:32:34 -0600

    chore: refactor ipfs settings

--- src/main/core/ipfs/browser/index.js
+++ src/main/core/ipfs/browser/index.js
@@ -6,11 +6,14 @@ const startRunning = async (repo) => {
     const defaultIPFS = {
         ...defaultConf(), ...{
             "Bootstrap": [
-                '/dns4/ec2-34-209-228-155.us-west-2.compute.amazonaws.com/tcp/4002/wss/p2p/QmSHSTNyKJ1EGVVKj7dKZFmxj9FaBfE7S23MNTj1Jwungg',
+                "/dns4/ec2-34-209-228-155.us-west-2.compute.amazonaws.com/tcp/4002/ws/p2p/QmSHSTNyKJ1EGVVKj7dKZFmxj9FaBfE7S23MNTj1Jwungg",
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
             ],
             "Addresses": {
                 "Swarm": [
-                    '/ip4/0.0.0.0/tcp/4010',
                     '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
                     '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
                     '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
@@ -24,7 +27,7 @@ const startRunning = async (repo) => {
         config: conf,
         start: true,
         preload: {enabled: false},
-        EXPERIMENTAL: {pubsub: true,},
+        EXPERIMENTAL: {pubsub: true, dht: false},
     })
 
     const ipfsID = await isInstance.id()
