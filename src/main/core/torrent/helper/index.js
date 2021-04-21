009445d1cdd66107e6a013f10f34724eb2acdb0c
phillmac <phillmac@users.noreply.github.com>
2021-04-22 08:07:22 +1000

    Update index.js

--- src/main/core/torrent/helper/index.js
+++ src/main/core/torrent/helper/index.js
@@ -9,6 +9,11 @@ module.exports = {
             return biggest.length > file.length ? biggest : file;
         });
     },
+    requiresTranscoding: (file) => {
+        return [".mkv", ".avi", ".wmv", ".webm", ".flv"].some((ext) =>
+        file.name.toLowerCase().endsWith(ext)
+      );
+    }
     calcChunkPercent: (downloaded, total) => {
         const targetLoadedSize = MIN_SIZE_LOADED > total ? total : MIN_SIZE_LOADED;
         const targetLoadedPercent = MIN_PERCENTAGE_LOADED * total / 100.0;
