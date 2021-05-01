504155da28c1180a0e45bb659fdaa7bcdb7f81de
geolffrey <gmjun2000@gmail.com>
2021-05-01 15:37:12 -0600

    chore: fix peering peer setting

--- src/main/core/ipfs/node/settings.js
+++ src/main/core/ipfs/node/settings.js
@@ -66,7 +66,7 @@ module.exports = () => {
         "Discovery": {"MDNS": {"Enabled": true, "Interval": 10}},
         "Peering": {
             "Peers": [
-                {"ID": "QmSHSTNyKJ1EGVVKj7dKZFmxj9FaBfE7S23MNTj1Jwungg", "Addrs": ["/ip4/34.209.228.155/tcp/4001"]},
+                {"ID": "QmSHSTNyKJ1EGVVKj7dKZFmxj9FaBfE7S23MNTj1Jwungg", "Addrs": ["/ip4/34.214.114.130/tcp/4001"]},
                 {"ID": "QmbPFTECrXd7o2HS2jWAJ2CyAckv3Z5SFy8gnEHKxxH52g", "Addrs": ["/ip4/144.172.69.157/tcp/4001"]},
                 {
                     "ID": "12D3KooWQw3vx2E4FKpL9GHC9BpFya1MXVUFEVBAQVhMDkreCqwF",
