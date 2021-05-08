1fb741858ae5a3c9d6cc5da2d49fdf3e96b982fb
geolffrey <gmjun2000@gmail.com>
2021-05-08 16:02:01 -0600

    feat: broadcasting support

--- src/main/core/broadcast.js
+++ src/main/core/broadcast.js
@@ -2,13 +2,14 @@ const log = require('logplease').create('BROADCAST')
 const EventEmitter = require('events')
 const PubSub = require('orbit-db-pubsub')
 
-
 module.exports = class Broadcast extends PubSub {
 
     constructor(...props) {
         super(...props);
         const [ipfs,] = props
         this._id = ipfs.peerId.id;
+        this._middlewares = {};
+
         log.warn('Broadcast running')
         // Extending EventEmitter
         this.__proto__ = Object.assign(
@@ -16,10 +17,32 @@ module.exports = class Broadcast extends PubSub {
         )
     }
 
+    _fromUint8Array(message) {
+        try {
+            return JSON.parse(
+                Buffer.from(message).toString()
+            )
+        } catch (e){
+            console.log(e);
+            return message
+        }
+    }
+
+    async _handleMessage(message) {
+        const parsedData = message.data
+        console.log(this._fromUint8Array(parsedData));
+
+        super._handleMessage(message)
+    }
 
     async multicast(topic) {
-        const currentPeers = await this.getPeers(topic)
+        // const currentPeers = await this.getPeers(topic)
 
+    }
+
+    addMiddleware(middleware) {
+        // Middleware class name as index
+        this._middlewares[middleware.name] = middleware
     }
 
     getPeers(topic) {
