dbdab82c58f7a1a79924bcf752dcaebeaf200884
geolffrey <gmjun2000@gmail.com>
2021-05-08 21:07:18 -0600

    fix: direct dial to workers, custom p2p

--- src/main/core/ipfs/browser/index.js
+++ src/main/core/ipfs/browser/index.js
@@ -1,16 +1,18 @@
 const IPFS = require('ipfs');
 const log = require('logplease').create('IPFS')
+const libp2p = require('./p2p')
 const defaultConf = require('./settings');
 
 
 const ipfsFactory = async (conf = {}) => {
 
     // Ipfs factory
+    const confSettings = defaultConf()
     const isInstance = await IPFS.create({
         ...{
-            config: defaultConf(), preload: {enabled: false},
-            EXPERIMENTAL: {pubsub: true, ipnsPubsub: true, dht: true},
-            libp2p: {config: {dht: {enabled: true}}},
+            config: confSettings, preload: {enabled: false},
+            EXPERIMENTAL: {pubsub: true},
+            ...{libp2p},
         }, ...conf
     })
 
@@ -19,6 +21,13 @@ const ipfsFactory = async (conf = {}) => {
         const ipfsID = await isInstance.id()
         isInstance.kill = async () => isInstance.stop(); // Alias to stop
         isInstance.peerId = ipfsID; // Add virtual attr needed for broadcasting
+
+        await Promise.all(confSettings.Bootstrap.map(async (p) => {
+            log.info('connecting', p)
+            return await isInstance.swarm.connect(p)
+        }))
+
+        setInterval(async () => console.log(await isInstance.swarm.peers()), 3000)
         log.info('Running ipfs id', ipfsID.id)
         return isInstance
     } catch (e) {
--- src/main/core/ipfs/browser/settings.js
+++ src/main/core/ipfs/browser/settings.js
@@ -1,6 +1,5 @@
 module.exports = () => {
     return {
-        "Discovery": {"MDNS": {"Enabled": true, "Interval": 10}},
         "Bootstrap": [
             "/dns4/watchitapp.site/tcp/4002/wss/p2p/QmSHSTNyKJ1EGVVKj7dKZFmxj9FaBfE7S23MNTj1Jwungg",
             "/dns4/direct.vps1.phillm.net/tcp/4002/wss/p2p/QmbPFTECrXd7o2HS2jWAJ2CyAckv3Z5SFy8gnEHKxxH52g",
@@ -8,6 +7,12 @@ module.exports = () => {
             "/dns4/direct.vps3.phillm.net/tcp/4002/wss/p2p/12D3KooWD4Z47R1pnzTxCVQAiTKTHasWU2xTAcffyC38BNKM68yw",
         ],
         "Addresses": {
+            "Delegates": [
+                "/dns4/node0.delegate.ipfs.io/tcp/443/https",
+                "/dns4/node1.delegate.ipfs.io/tcp/443/https",
+                "/dns4/node2.delegate.ipfs.io/tcp/443/https",
+                "/dns4/node3.delegate.ipfs.io/tcp/443/https"
+            ],
             "Swarm": [
                 '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
                 '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
