ef0c8ff628da9a66519168bc72d335813a2ad345
geolffrey <gmjun2000@gmail.com>
2021-05-24 16:49:12 -0600

    refactor: clean and re-structure

--- src/render/core/app/components/app-background/index.jsx
+++ src/render/core/app/components/app-background/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import Logo from 'components/util-header-logo/'
+import Logo from 'components/app-logo/'
 
 export default class Background extends React.Component {
 
