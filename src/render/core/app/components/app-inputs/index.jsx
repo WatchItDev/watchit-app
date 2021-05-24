fff43410c243d6cb76044814add5842caa239987
jadapema <Ruperma040193>
2021-05-24 15:37:56 -0600

    refactor: inputs to styled components

--- src/render/core/app/components/app-inputs/index.jsx
+++ src/render/core/app/components/app-inputs/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
 
 export default class BoxInput extends React.PureComponent {
 
     constructor(props) {
         super(props);
         this.ref = null;
+        this.state = {
+            invalid: false
+        };
     }
 
     static get defaultProps() {
@@ -14,7 +18,6 @@ export default class BoxInput extends React.PureComponent {
             autoComplete: 'off'
         }
     }
-
 
     static get propTypes() {
         return {
@@ -25,7 +28,7 @@ export default class BoxInput extends React.PureComponent {
     onInput = (e) => {
         //If handler
         if (this.props.onInput) this.props.onInput(e);
-        e.target.classList.remove("invalid");
+        this.setState({invalid: false});
     }
 
     onChange = (e) => {
@@ -42,46 +45,61 @@ export default class BoxInput extends React.PureComponent {
 
     onInvalid = (e) => {
         e.preventDefault();
-        e.target.classList.add("invalid");
+        this.setState({invalid: true});
     }
 
     getRef = (ref) => {
         this.ref = ref
     }
 
-
     render() {
         return (
-            <div>
+            <InputContainer>
                 {this.props.icon && <i className={this.props.icon + " gray-text"}/>}
-                <input {...this.props}
+                <Input {...this.props}
                        onInput={this.onInput}
                        onChange={this.onChange}
                        onKeyDown={this.onKeyDown}
                        onInvalid={this.onInvalid}
                        ref={this.getRef}
-                       className="white-text validate"
+                       invalid={this.state.invalid}
                 />
-            </div>
+            </InputContainer>
         )
     }
 }
 
+const InputContainer = styled.div`
+  width: 100%;
+  border-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.5) !important;
+  padding: 0 0.75rem;
+  position: relative;
+`;
 
-export class BoxTextArea extends BoxInput {
-    render() {
-        return (
-            <div>
-                {this.props.icon && <i className={this.props.icon + " gray-text"}/>}
-                <textarea {...this.props}
-                          onInput={this.onInput}
-                          onChange={this.onChange}
-                          onKeyDown={this.onKeyDown}
-                          onInvalid={this.onInvalid}
-                          ref={this.getRef}
-                          className="white-text validate"
-                />
-            </div>
-        )
-    }
-}
+const Input = styled.input`
+  background-color: transparent;
+  border: none;
+  border-radius: 0;
+  outline: none;
+  height: 3rem;
+  width: 100%;
+  font-size: 1rem;
+  padding: 0;
+  box-sizing: content-box;
+  transition: all .3s;
+  color: #fff;
+  margin: 0 !important;
+  border-bottom: 1px solid transparent !important;
+  border-bottom: ${props => props.invalid ? '1px solid #F44336 !important' : 'none'};
+  box-shadow: ${props => props.invalid ? '0 1px 0 0 #F44336 !important' : 'none'};
+  
+  &:focus {
+    border-bottom: 1px solid #26a69a !important;
+    box-shadow: 0 1px 0 0 #26a69a !important;
+  }
+  
+  &::placeholder {
+    color: #999;
+  }
+`;
