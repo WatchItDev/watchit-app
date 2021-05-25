c0719a22c2cf9ba06c9650ae99739786ee419ff5
geolffrey <gmjun2000@gmail.com>
2021-05-24 20:08:34 -0600

    refactor: clean and re-structure

--- src/render/core/app/components/app-logo/index.jsx
+++ src/render/core/app/components/app-logo/index.jsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import setting from 'settings'
 import icon from 'render/media/icons/icon.png'
-import styled, {keyframes} from 'styled-components';
+import styled, { keyframes } from 'styled-components'
 
-export default class Logo extends React.Component {
+export default function Logo (props = {
+  show: true,
+  absolute: false,
+  thumbnail: true,
+}) {
 
-    static get defaultProps() {
-        return {
-            show: true,
-            absolute: false,
-            thumbnail: true,
-        }
-    }
+  return (
+    <LogoWrapper show={props.show} absolute={props.absolute} thumbnail={props.thumbnail}>
+      <LogoImg src={icon} alt="Logo" width={50} height={50}/>
+      <LogoText>ATCH</LogoText>
+      <LogoColoredText>IT</LogoColoredText>
+    </LogoWrapper>
+  )
 
-    render() {
-        return (
-            <LogoWrapper show={this.props.show} absolute={this.props.absolute} thumbnail={this.props.thumbnail}>
-                <LogoImg src={icon} alt="Logo" width={50} height={50}/>
-                <LogoText>ATCH</LogoText>
-                <LogoColoredText>IT</LogoColoredText>
-            </LogoWrapper>
-        )
-    }
 }
 
 const LogoWrapper = styled.h5`
@@ -30,19 +25,19 @@ const LogoWrapper = styled.h5`
   letter-spacing: 2px;
   font-family: "Oswald", Arial, sans-serif;
   display: ${props => props.show ? 'inline-block' : 'none'};
-  margin: 0;  
+  margin: 0;
   @media ${setting.styles.devices.mobileS} {
     font-size: 1.5rem !important;
   }
-  
+
   @media ${setting.styles.devices.laptop} {
     font-size: 2rem !important;
   }
-  
+
   @media ${setting.styles.devices.desktop} {
     font-size: 3rem !important;
   }
-`;
+`
 
 const LogoImg = styled.img`
   width: 50px;
@@ -53,17 +48,17 @@ const LogoImg = styled.img`
     width: 40px;
     height: 40px;
   }
-  
+
   @media (min-width: 992px) {
     width: 50px;
     height: 50px;
   }
-  
+
   @media (min-width: 2000px) {
     width: 70px;
     height: 70px;
   }
-`;
+`
 
 const LogoText = styled.span`
   position: relative;
@@ -74,7 +69,7 @@ const LogoText = styled.span`
   @media (min-width: 2000px) {
     top: -12px;
   }
-`;
+`
 
 const Colors = keyframes`
   0% {
@@ -92,15 +87,15 @@ const Colors = keyframes`
   100% {
     color: rgb(251, 211, 1);
   }
-`;
+`
 
 const LogoColoredText = styled.span`
   font-weight: bold;
   position: relative;
   top: -9px;
   animation: ${Colors} 4.6s ease infinite;
-  
+
   @media (min-width: 2000px) {
     top: -12px;
   }
-`;
+`
