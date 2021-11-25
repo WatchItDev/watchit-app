43c5ce62d9cdf677c50726978918ec3288e11a58
gmena <gmjun2000@gmail.com>
2021-11-25 01:18:01 -0600

    fix: typo

--- src/render/core/helpers/gateway.js
+++ src/render/core/helpers/gateway.js
@@ -3,14 +3,14 @@ import settings from '@settings'
 export default {
 
   _builtPath (resource) {
-    let builtPath = resource.cid
+    let builtPath = resource.route
     builtPath = 'quality' in resource ? `${builtPath}/${resource.quality}` : builtPath
     builtPath = 'index' in resource ? `${builtPath}/${resource.index}` : builtPath
     return builtPath
   },
 
   dummyParse (resource) {
-    if (!('route' in resource)) throw new Error('Resource object need at least `cid`')
+    if (!('route' in resource)) throw new Error('Resource object need at least `route`')
     const builtPath = this._builtPath(resource) // Build path using index and optional quality
     const random = Math.floor(Math.random() * settings.gateways.length)
     return `${settings.gateways[random]}/ipfs/${builtPath}`
