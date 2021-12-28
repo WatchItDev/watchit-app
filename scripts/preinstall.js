c340cce6d79275292b7163f3e82cd3b0c63b3719
gmena <gmjun2000@gmail.com>
2021-12-28 10:53:41 -0600

    BREAKING CHANGES: chore: upgrade go-ipfs==0.10.0 && js-ipfs==0.61.0 && orbitdb==0.28.0

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -5,7 +5,7 @@ const executePreInstall = async () => {
   log.info('Installing on', osType)
   try {
     // Try `npm cache clean --force` if packages isn't installed
-    await execPassthru('npm install go-ipfs@0.6.0 --no-save', await getElevateExec())
+    await execPassthru('npm install go-ipfs@0.11.0 --no-save', await getElevateExec())
   } catch (err) {
     log.error(err)
   }
