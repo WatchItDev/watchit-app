2dd6a169a022a4f9c01a77a624e3dba009d018ad
gmena <gmjun2000@gmail.com>
2022-01-04 12:14:47 -0600

    fix: image test

--- src/render/core/app/components/Image/index.test.jsx
+++ src/render/core/app/components/Image/index.test.jsx
@@ -5,21 +5,17 @@ import Image from './index'
 
 /* eslint-disable no-undef */
 describe('<Image />', () => {
-  const props = { index: 'image.jpg', route: 'QmcdLW9p1dcYYKBHZdRXEXA4go6Qd3C4ce12khyiCqVNaH' }
+  const src = 'https://gateway.ipfs.io/ipfs/QmcdLW9p1dcYYKBHZdRXEXA4go6Qd3C4ce12khyiCqVNaH/image/medium.jpg'
 
   it('should render', () => {
-    const imageComponent = shallow(<Image src={props} />)
+    const imageComponent = shallow(<Image src={src} />)
     expect(imageComponent).toMatchSnapshot()
   })
 
-  it('should render with absolute `route`', () => {
-    const imageComponent = shallow(<Image src={{ route: props.route }} />)
-    expect(imageComponent).toMatchSnapshot()
-  })
 
   // eslint-disable-next-line no-undef
   it('should render with random gateway', () => {
-    const imageComponent = mount(<Image src={props} />)
+    const imageComponent = mount(<Image src={src} />)
     const imageEl = imageComponent.instance().img
     // Image link built using at least one gateway
     const match = settings.gateways.some(gateway => ~imageEl.src.indexOf(gateway))
@@ -28,35 +24,14 @@ describe('<Image />', () => {
   })
 
   // eslint-disable-next-line no-undef
-  it('should render with relative `route` and `index`', () => {
-    const imageComponent = shallow(<Image src={props} />)
-    const componentProps = imageComponent.instance().props
-
-    /* eslint-disable no-undef */
-    expect(componentProps.src).toBeInstanceOf(Object)
-    expect(componentProps.src.route).toBe(props.route)
-    expect(componentProps.src.index).toBe(props.index)
-  })
-
-  // eslint-disable-next-line no-undef
   it('should not render with empty src', () => {
     try {
-      const emptyObj = {}
-      shallow(<Image src={emptyObj} />)
+      shallow(<Image src={""} />)
     } catch (e) {
       // eslint-disable-next-line no-undef
       expect(e).toBeInstanceOf(Error)
     }
   })
 
-  // eslint-disable-next-line no-undef
-  it('should not render without `route`', () => {
-    try {
-      const withoutRoute = { index: 'image.jpg' }
-      shallow(<Image src={withoutRoute} />)
-    } catch (e) {
-      // eslint-disable-next-line no-undef
-      expect(e).toBeInstanceOf(Error)
-    }
-  })
+
 })
