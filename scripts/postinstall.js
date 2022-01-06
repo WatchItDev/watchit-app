97697f2c009a3f2344f00ae92c5f7bf1bf17e2d2
gmena <gmjun2000@gmail.com>
2022-01-06 10:16:06 -0600

    fix: peer-deps

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,15 @@
-const {execPassthru} = require('./util')
+const log = require('logplease').create('PREINSTALL')
+const { execPassthru, getElevateExec, osType } = require('./util')
+
 ;(async () => {
-    await execPassthru('electron-builder install-app-deps')
-    await execPassthru('npm rebuild ursa-optional')
+  log.info('Installing on', osType)
+  try {
+    // Try `npm cache clean --force` if packages isn't installed
+    await execPassthru('npm install go-ipfs@0.10.0 --no-save', await getElevateExec())
+    await execPassthru('npm install wrtc@0.4.7 ipfs@0.60.2 level-js@6.1.0 --no-save', await getElevateExec())
+    await execPassthru('npm install blockstore-core@1.0.2 blockstore-datastore-adapter@2.0.2 --no-save', await getElevateExec())
+  } catch (err) {
+    log.error(err)
+  }
 })()
 
