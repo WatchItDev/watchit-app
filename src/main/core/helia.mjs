2d7780f825324982047aa22eedf4cdd6662d3950
Geolffrey Mena <gmjun2000@gmail.com>
2024-06-06 13:00:04 -0600

    feat: multi channel

--- src/main/core/helia.mjs
+++ src/main/core/helia.mjs
@@ -2,11 +2,6 @@ import logplease from "logplease";
 import { createHelia, libp2pDefaults } from "helia";
 import { unixfs } from "@helia/unixfs";
 
-import {
-  createDelegatedRoutingV1HttpApiClient,
-  DelegatedRoutingV1HttpApiClient,
-} from '@helia/delegated-routing-v1-http-api-client'
-
 import { tcp } from "@libp2p/tcp";
 import { gossipsub } from "@chainsafe/libp2p-gossipsub";
 import { webSockets } from "@libp2p/websockets";
@@ -14,10 +9,14 @@ import { webRTC, webRTCDirect } from "@libp2p/webrtc";
 import { webTransport } from '@libp2p/webtransport'
 import { circuitRelayTransport } from "@libp2p/circuit-relay-v2";
 import { pubsubPeerDiscovery } from '@libp2p/pubsub-peer-discovery'
+import { peerIdFromString } from '@libp2p/peer-id'
+import { createDelegatedRoutingV1HttpApiClient } from '@helia/delegated-routing-v1-http-api-client'
+import { bootstrap } from '@libp2p/bootstrap'
 
 const log = logplease.create("HELIA");
+const PUBSUB_PEER_DISCOVERY = 'watchit-pubsub-discovery';
 
-function getConfig(runtime = "node") {
+function getConfig(runtime = "node", bootstrapNodes = []) {
   const isBrowserRuntime = runtime === "web";
   const defaults = libp2pDefaults();
 
@@ -30,11 +29,20 @@ function getConfig(runtime = "node") {
       "/ip4/0.0.0.0/tcp/0/ws",
       "/webrtc",
     ]) || [
-      "/webrtc",
-      // "/wss",
-      "/ws",
-    ]),
+        "/webrtc",
+      ]),
   ];
+
+  const peerDiscovery = [
+    ...defaults.peerDiscovery,
+    ...(isBrowserRuntime && [
+      pubsubPeerDiscovery({
+        interval: 10_000,
+        topics: [PUBSUB_PEER_DISCOVERY],
+        listenOnly: false,
+      })
+    ] || [])
+  ]
 
   const transports = [
     ...((!isBrowserRuntime && [
@@ -42,20 +50,20 @@ function getConfig(runtime = "node") {
       circuitRelayTransport({ discoverRelays: 3 }),
       webSockets({ websocket: { rejectUnauthorized: false } }),
     ]) || [
-      webTransport(),
-      webSockets(),
-      circuitRelayTransport({ discoverRelays: 0 }),
-      webRTC({
-        rtcConfiguration: {
-          iceServers: [
-            {
-              // STUN servers help the browser discover its own public IPs
-              urls: ['stun:stun.l.google.com:19302', 'stun:global.stun.twilio.com:3478'],
-            },
-          ],
-        },
-      }), 
-    ]),
+        webTransport(),
+        webSockets(),
+        circuitRelayTransport({ discoverRelays: 1 }),
+        webRTC({
+          rtcConfiguration: {
+            iceServers: [
+              {
+                // STUN servers help the browser discover its own public IPs
+                urls: ['stun:stun.l.google.com:19302', 'stun:global.stun.twilio.com:3478'],
+              },
+            ],
+          },
+        }),
+      ]),
     webRTCDirect(), // both runtimes
   ];
 
@@ -64,6 +72,7 @@ function getConfig(runtime = "node") {
     libp2p: Object.assign({}, defaults, {
       transports,
       addresses: { listen },
+      peerDiscovery,
       services: {
         ...defaults.services,
         pubsub: gossipsub({
@@ -76,6 +85,7 @@ function getConfig(runtime = "node") {
   };
 }
 
+
 // Tricky implementation to allow handle helia in electron and browser
 // Helia is an ESM-only module but Electron currently only supports CJS
 // at the top level, so we have to use dynamic imports to load it
@@ -84,11 +94,6 @@ export async function Helia(runtime) {
   const node = await createHelia(config);
   const fs = unixfs(node);
   log.info(`Running helia with peer ${node.libp2p.peerId}`);
-
-  // uncomment for print peer connecting info
-  // node.libp2p.addEventListener("peer:connect", (ev) => {
-  //   console.log("[peer:connect]", ev.detail);
-  // });
 
   return {
     node,
