47bfa5d5c697e9ec4bb90c97b994e671c0698177
Geolffrey Mena <gmjun2000@gmail.com>
2024-06-28 14:35:37 -0600

    chore: added resources

--- src/preload/index.js
+++ src/preload/index.js
@@ -13,10 +13,12 @@ import { electronAPI } from '@electron-toolkit/preload'
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
+    contextBridge.exposeInMainWorld('electron', electronAPI)
     contextBridge.exposeInMainWorld('ipc', electronAPI.ipcRenderer)
   } catch (error) {
     console.error(error)
   }
 } else {
+  window.electron = electronAPI
   window.ipc = electronAPI.ipcRenderer
 }
