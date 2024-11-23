8b7822b8189b06937908c827363f57d3dde8e549
jadapema <jadapema@gmail.com>
2024-11-23 17:53:20 -0600

    feat: here the profile creation is working

--- src/auth/context/lens/authProvider.tsx
+++ src/auth/context/lens/authProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { development, LensConfig, LensProvider } from '@lens-protocol/react-web';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { bindings } from '@lens-protocol/wagmi';
@@ -6,9 +6,13 @@ import { WagmiProvider, createConfig, http } from 'wagmi';
 import { AuthContextProvider } from './authContext';
 import { AuthProviderProps } from './types';
 import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
+import { WALLET_ADAPTERS } from '@web3auth/base';
 import { Web3Auth } from "@web3auth/modal";
 import { web3AuthOptions } from '@src/auth/web3AuthServices/web3authContext.tsx';
 import { polygonAmoy } from "wagmi/chains";
+// import { MetamaskAdapter } from "@web3auth/metamask-adapter";
+// import { chain } from '@src/auth/config/chainConfig.ts';
+// import { getInjectedAdapters } from "@web3auth/default-evm-adapter";
 
 const web3AuthInstance = new Web3Auth(web3AuthOptions);
 
@@ -19,20 +23,106 @@ const web3AuthInstance = new Web3Auth(web3AuthOptions);
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const queryClient = new QueryClient();
 
-  useEffect(() => {
-    const configureWeb3Auth = async () => {
-      try {
-        await web3AuthInstance.initModal();
-      } catch (error) {
-        console.error('Error inicializando Web3Auth:', error);
-      }
-    };
+  const modalConfig = {
+    [WALLET_ADAPTERS.AUTH]: {
+      label: "openlogin",
+      loginMethods: {
+        facebook: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "facebook login",
+          showOnModal: false,
+        },
+        reddit: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "reddit login",
+          showOnModal: false,
+        },
+        discord: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "discord login",
+          showOnModal: false,
+        },
+        twitch: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "twitch login",
+          showOnModal: false,
+        },
+        apple: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "apple login",
+          showOnModal: false,
+        },
+        line: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "line login",
+          showOnModal: false,
+        },
+        github: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "github login",
+          showOnModal: false,
+        },
+        kakao: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "kakao login",
+          showOnModal: false,
+        },
+        linkedin: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "linkedin login",
+          showOnModal: false,
+        },
+        twitter: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "twitter login",
+          showOnModal: false,
+        },
+        wechat: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "wechat login",
+          showOnModal: false,
+        },
+        weibo: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "weibo login",
+          showOnModal: false,
+        },
+        farcaster: {
+          // it will hide the facebook option from the Web3Auth modal.
+          name: "farcaster login",
+          showOnModal: false,
+        }
+      },
+      // setting it to false will hide all social login methods from modal.
+      showOnModal: true,
+    },
+  }
 
-    configureWeb3Auth();
-  }, []);
+  // THIS CODE CONNECT METAMASK TO WEB3AUTH
+  // const metamaskAdapter = new MetamaskAdapter({
+  //   clientId,
+  //   sessionTime: 86400, // 1 hour in seconds
+  //   web3AuthNetwork: "sapphire_devnet",
+  //   chainConfig: chain.polygonAmoy,
+  // })
+  //
+  // web3AuthInstance.configureAdapter(metamaskAdapter);
 
-  const web3AuthConnector = new Web3AuthConnector({
-    web3AuthInstance
+  // THERE IS ALSO THIS OPTION TO ADD WALLETS TO WEB3AUTH
+
+  // const setUpInjectedAdapters = async () => {
+  //   const adapters = await getInjectedAdapters({ options: web3AuthOptions });
+  //
+  //   adapters.forEach((adapter) => {
+  //     web3AuthInstance.configureAdapter(adapter);
+  //   });
+  // }
+  //
+  // setUpInjectedAdapters();
+
+  const web3AuthConnector = Web3AuthConnector({
+    web3AuthInstance,
+    modalConfig
   });
 
   const wagmiConfig = useMemo(
@@ -45,6 +135,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
         transports: {
           [polygonAmoy.id]: http(),
         },
+        // autoConnect: true
       }),
     []
   );
