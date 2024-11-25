767d65da44004a39c38ec7f911f3b847a167ed5c
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-11-25 12:46:21 -0600

    refactor: enhance user profile SEO with OG and Twitter meta tags
    - src/pages/dashboard/user/profile.tsx:
      - Changed relative import of `useParams` to absolute import.
      - Added dynamic OG meta tags for title, description, image, and URL based on user ID.
      - Included Twitter meta tags for enhanced social sharing, utilizing the user ID for context specificity.

--- src/pages/dashboard/user/profile.tsx
+++ src/pages/dashboard/user/profile.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 // sections
 import { UserProfileView } from '@src/sections/user/view';
-import { useParams } from '../../../routes/hooks';
+import { useParams } from '@src/routes/hooks';
 
 // ----------------------------------------------------------------------
 
@@ -12,7 +12,24 @@ export default function UserProfilePage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard: User Profile</title>
+        <title> Dashboard: User Profile | {id}</title>
+        {/*OG*/}
+        <meta property="og:title" content={`Watchit Movie | ${id}`} />
+        <meta
+          property="og:description"
+          content="Visit my profile in Watchit Movie | Discover stories, art, and experiences like never before, connecting you with creators in a decentralized, space built just for you."
+        />
+        <meta property="og:image" content={`https://app.watchit.movie/default.jpg`} />
+        <meta property="og:url" content={`https://app.watchit.movie/${id}`} />
+        {/*Twitter*/}
+        <meta name="twitter:title" content={`Watchit Movie: A New Era of Video Content with Web3 x AI | ${id}`} />
+        <meta
+          name="twitter:description"
+          content="Visit my profile in Watchit Movie | Discover stories, art, and experiences like never before, connecting you with creators in a decentralized, space built just for you."
+        />
+        <meta name="twitter:image" content={`https://app.watchit.movie/default.jpg`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@app_watchit" />
       </Helmet>
 
       <UserProfileView id={id} />
