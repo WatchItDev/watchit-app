7d723fff5dc7abc7ff71a12832c413f6df74a4b6
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-12-01 17:59:39 -0600

    feat(profile-home): enhance publications layout handling
    - Add `useRef` to manage parent container width more efficiently.
    - Implement `ResizeObserver` for dynamic item adjustment on resize.
    - Introduce `rowsToShow` and `allItemsShown` states for "show more" functionality.
    - Limit displayed publications to 10 to optimize performance.
    - Refactor item layout to use CSS grid for flexible column handling.

--- src/sections/user/profile-home.tsx
+++ src/sections/user/profile-home.tsx
@@ -1,82 +1,142 @@
-// ProfileHome.js
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { ProfilePublicationItem } from './profile-publication-item';
+import IconButton from "@mui/material/IconButton";
+import { IconCaretDown, IconCaretUp } from "@tabler/icons-react";
 
 interface ProfileHomeProps {
   publications: any;
-  noPaddings?: boolean
+  noPaddings?: boolean;
 }
 
-export default function ProfileHome({ publications, noPaddings = false}: ProfileHomeProps) {
-  const minItemWidth = 150;
-  const maxItemWidth = 200;
-  const gap = 16; // Espacio entre ítems
+export default function ProfileHome({ publications, noPaddings = false }: ProfileHomeProps) {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const minItemWidth = 200;
+  const maxItemWidth = 250;
+  const gap = 10; // Space between items
+  const [itemsPerRow, setItemsPerRow] = useState(4); // Default items per row
+  const [rowsToShow, setRowsToShow] = useState(1);
+  const [allItemsShown, setAllItemsShown] = useState(false);
 
-  const calculateItemWidth = () => {
-    const containerWidth = window.innerWidth;
-    const itemsPerRow = Math.floor((containerWidth + gap) / (maxItemWidth + gap));
-    const itemWidth = (containerWidth - (itemsPerRow - 1) * gap) / itemsPerRow;
+  const calculateItemsPerRow = (parentWidth: number) => {
+    let maxItems = Math.floor(parentWidth / minItemWidth);
+    let minItems = Math.floor(parentWidth / maxItemWidth);
+    let items = maxItems;
 
-    return Math.min(Math.max(itemWidth, minItemWidth), maxItemWidth);
+    while (items >= minItems) {
+      const itemWidth = parentWidth / items;
+      if (itemWidth >= minItemWidth && itemWidth <= maxItemWidth) {
+        break;
+      }
+      items--;
+    }
+
+    if (items < 1) items = 1;
+
+    return items;
   };
 
-  const [itemWidth, setItemWidth] = useState(calculateItemWidth());
+  useEffect(() => {
+    if (!parentRef.current) return;
+
+    const observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const parentWidth = entry.contentRect.width;
+        const items = calculateItemsPerRow(parentWidth);
+        setItemsPerRow(items);
+      }
+    });
+
+    observer.observe(parentRef.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [minItemWidth, maxItemWidth]);
 
   useEffect(() => {
-    const handleResize = () => setItemWidth(calculateItemWidth());
+    if (parentRef.current) {
+      const parentWidth = parentRef.current.offsetWidth;
+      const items = calculateItemsPerRow(parentWidth);
+      setItemsPerRow(items);
+    }
+  }, [minItemWidth, maxItemWidth]);
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  // Only take 9 publications to avoid performance issues and UI problems
+  const displayedPublications = publications ? publications.slice(0, 10) : [];
 
-  console.log('publications')
-  console.log(publications)
+  const handleShowMore = () => {
+    if (allItemsShown) {
+      setRowsToShow(1);
+      setAllItemsShown(false);
+    } else {
+      setRowsToShow((prevRows) => {
+        const newRows = prevRows + 1;
+        if (newRows * itemsPerRow >= displayedPublications.length) {
+          setAllItemsShown(true);
+        }
+        return newRows;
+      });
+    }
+  };
+
+  const publicationsToShow = displayedPublications.slice(0, rowsToShow * itemsPerRow);
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: `${gap}px`,
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-        padding: noPaddings ? 0 : 2,
-      }}
-    >
-      {publications?.map((publication) => (
-        <Box
-          key={publication.id}
-          sx={{
-            // display: { xs: 'block', sm: 'flex' },
-            // gap: `${gap}px`,
-            flex: `0 1 ${itemWidth}px`,
-            maxWidth: `${maxItemWidth}px`,
-          }}
-        >
-          {/*<ProfilePublicationItem publication={publication} />*/}
-          <ProfilePublicationItem publication={publication} />
-        </Box>
-      ))}
+    <>
+      <Box
+        ref={parentRef}
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(auto-fill, minmax(${minItemWidth}px, 1fr))`,
+          flexWrap: 'wrap',
+          gap: `${gap}px`,
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          padding: noPaddings ? 0 : 2,
+        }}
+      >
+        {publicationsToShow.map((publication) => (
+          <Box
+            key={publication.id}
+            sx={{
+              display: 'grid',
+              gap: `${gap}px`,
+              gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
+            }}
+          >
+            <ProfilePublicationItem publication={publication} />
+          </Box>
+        ))}
 
-      {!publications?.length && (
-        <Typography
-          sx={{
-            height: '20rem',
-            textAlign: 'center',
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            fontWeight: 'bold',
-            background: '#2b2d31',
-            borderRadius: '1rem',
-          }}
-        >
-          This profile has no posts
-        </Typography>
-      )}
-    </Box>
+        {!publicationsToShow.length && (
+          <Typography
+            sx={{
+              height: '20rem',
+              textAlign: 'center',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              fontWeight: 'bold',
+              background: '#2b2d31',
+              borderRadius: '1rem',
+            }}
+          >
+            This profile has no posts
+          </Typography>
+        )}
+      </Box>
+      <Box>
+        {publicationsToShow.length < displayedPublications.length || allItemsShown ? (
+          <>
+            <IconButton onClick={handleShowMore} sx={{ mt: 2 }}>
+              {allItemsShown ? <IconCaretUp /> : <IconCaretDown />}
+            </IconButton>
+          </>
+        ) : null}
+      </Box>
+    </>
   );
 }
