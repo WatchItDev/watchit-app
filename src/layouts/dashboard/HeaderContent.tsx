bbb00fc524896cbd3588559c610868ae3d222efc
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-12-03 17:05:08 -0600

    fix(HeaderContent): adjust button spacing and refactor logic
    - Remove unnecessary `IconButton` and streamline the back button logic.
    - Standardize spacing and alignment for better readability and maintainability.
    - Modify `sx` properties of `Button` for improved visual consistency.

--- src/layouts/dashboard/HeaderContent.tsx
+++ src/layouts/dashboard/HeaderContent.tsx
@@ -1,53 +1,53 @@
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import {IconChevronLeft} from "@tabler/icons-react";
+import { IconChevronLeft } from "@tabler/icons-react";
 import Typography from "@mui/material/Typography";
 import Label from "@src/components/label";
-import {FC} from "react";
-import {useResponsive} from "@src/hooks/use-responsive.ts";
+import { FC } from "react";
+import { useResponsive } from "@src/hooks/use-responsive.ts";
 
 interface HeaderContentProps {
   handleBack?: () => void;
   title?: string;
 }
 
-const HeaderContent: FC<HeaderContentProps> = ({handleBack, title}) => {
+const HeaderContent: FC<HeaderContentProps> = ({ handleBack, title }) => {
   const mdUp = useResponsive('up', 'md');
 
   if (!mdUp) return <></>;
 
   return (
     <>
-      { handleBack ? (
+      {handleBack && (
         <Button
-          onClick={handleBack} disableFocusRipple
+          onClick={handleBack}
+          disableFocusRipple
+          startIcon={<IconChevronLeft size={20} />}
           sx={{
-            display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
             backgroundColor: '#24262A',
             borderRadius: 1.5,
             m: 1,
-            p: 0.2,
+            gap: 1,
+            p: 1,
             '&:hover': {
               backgroundColor: '#1E1F22'
             }
           }}
         >
-          <IconButton disableRipple>
-            <IconChevronLeft size={20} />
-            <Typography sx={{ ml: 1 }} variant='subtitle2'>Back</Typography>
-          </IconButton>
+          <Typography variant='subtitle2'>Back</Typography>
           {mdUp && <Label sx={{ px: 0.75, mr: 1, fontSize: 12, color: 'text.secondary' }}>Esc</Label>}
         </Button>
-      ) : <></> }
+      )}
 
-      { mdUp ? (
+      {mdUp && (
         <Typography variant="h6" sx={{ ml: 2 }}>
           {title}
         </Typography>
-      ):<></>
-      }
+      )}
     </>
-  )
+  );
 }
 
 export default HeaderContent;
