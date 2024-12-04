10ebf670062be25d5b9782bb8e1f87e73d966f89
Geolffrey Mena <gmjun2000@gmail.com>
2024-12-04 10:36:08 -0600

    refactor: wagmi

--- src/auth/context/web3Auth/authProvider.tsx
+++ src/auth/context/web3Auth/authProvider.tsx
@@ -1,68 +1,37 @@
 import React, { useMemo } from 'react';
 import { development, LensConfig, LensProvider } from '@lens-protocol/react-web';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { bindings } from '@lens-protocol/wagmi';
 import { WagmiProvider, createConfig, http } from 'wagmi';
-import { AuthContextProvider } from './authContext';
-import { AuthProviderProps } from './types';
-import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
-import { Web3Auth } from "@web3auth/modal";
-import { web3AuthOptions, modalConfig } from '@src/auth/context/web3Auth/config/web3authSettings.tsx';
-import { polygonAmoy } from "wagmi/chains";
-// import { MetamaskAdapter } from "@web3auth/metamask-adapter";
-// import { chain } from '@src/auth/config/chainConfig.ts';
-// import { getInjectedAdapters } from "@web3auth/default-evm-adapter";
+import { bindings } from '@lens-protocol/wagmi';
 
-const web3AuthInstance = new Web3Auth(web3AuthOptions);
+import { Web3Auth } from "@web3auth/modal";
+import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
+
+import { wagmi } from "./chainConfig.ts";
+import { AuthProviderProps } from './types';
+import { AuthContextProvider } from './authContext';
+import { web3AuthOptions, modalConfig } from './config/web3authSettings';
 
 /**
  * AuthProvider is a higher-order component that wraps the application with necessary providers
  * for state management, wallet connection, and Lens Protocol integration.
  */
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const web3AuthInstance = new Web3Auth(web3AuthOptions);
   const queryClient = new QueryClient();
 
-  // THIS CODE CONNECT METAMASK TO WEB3AUTH
-  // const metamaskAdapter = new MetamaskAdapter({
-  //   clientId,
-  //   sessionTime: 86400, // 1 hour in seconds
-  //   web3AuthNetwork: "sapphire_devnet",
-  //   chainConfig: chain.polygonAmoy,
-  // })
-  //
-  // web3AuthInstance.configureAdapter(metamaskAdapter);
-
-  // THERE IS ALSO THIS OPTION TO ADD WALLETS TO WEB3AUTH
-
-  // const setUpInjectedAdapters = async () => {
-  //   const adapters = await getInjectedAdapters({ options: web3AuthOptions });
-  //
-  //   adapters.forEach((adapter) => {
-  //     web3AuthInstance.configureAdapter(adapter);
-  //   });
-  // }
-  //
-  // setUpInjectedAdapters();
-
+  // wagmi web3 auth 
   const web3AuthConnector = Web3AuthConnector({
     web3AuthInstance,
     modalConfig
   });
 
-  const wagmiConfig = useMemo(
-    () =>
-      createConfig({
-        connectors: [
-          web3AuthConnector,
-        ],
-        chains: [polygonAmoy],
-        transports: {
-          [polygonAmoy.id]: http(),
-        },
-        syncConnectedChain: true
-      }),
-    [web3AuthConnector]
-  );
+  const wagmiConfig = createConfig({
+    syncConnectedChain: true,
+    chains: [wagmi.polygonAmoy],
+    connectors: [web3AuthConnector],
+    transports: { [wagmi.polygonAmoy.id]: http(), }
+  })
 
   const lensConfig: LensConfig = {
     environment: development,
--- src/auth/context/web3Auth/config/chainConfig.ts
+++ src/auth/context/web3Auth/config/chainConfig.ts
@@ -14,9 +14,8 @@ export const chain = {
   // },
   polygonAmoy: {
     chainNamespace: CHAIN_NAMESPACES.EIP155,
+    rpcTarget: process.env.VITE_RPC_ALCHEMY,
     chainId: "0x13882",
-    rpcTarget: "https://rpc-amoy.polygon.technology",
-    // rpcTarget: "https://polygon-amoy.g.alchemy.com/v2/Qa8X9PrNgKT6PJFYk3j3-32riKQ5nKp5",
     displayName: "Polygon Amoy Testnet",
     blockExplorerUrl: "https://www.oklink.com/amoy",
     ticker: "POL",
