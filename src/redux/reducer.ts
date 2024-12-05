0a76311f6f8eb026ff638568df84ad9a8fd51d54
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-12-05 13:23:08 -0600

    feat: add address reducer to redux store
    - **src/redux/reducer.ts**: Integrated `addressReducer` into the `appReducer` to manage state related to address within the store.
    - **src/redux/address/index.ts**: Implemented a new slice using Redux Toolkit to handle address state, including actions for setting and unsetting the address.

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import minibarReducer from '@redux/minibar/index';
 import drawerReducer from '@redux/drawer/index';
 import authReducer from '@redux/auth/index';
+import addressReducer from '@redux/address/index';
 
 const appReducer = combineReducers({
   minibar: minibarReducer,
   drawer: drawerReducer,
   auth: authReducer,
+  address: addressReducer
 });
 
 const rootReducer = (state: any, action: any) => {
