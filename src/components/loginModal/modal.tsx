2e83daab339f1cc008b6f850179e9fd642025edb
Geolffrey Mena <gmjun2000@gmail.com>
2024-12-05 11:56:16 -0600

    changes: fixed profile selection

--- src/components/loginModal/modal.tsx
+++ src/components/loginModal/modal.tsx
@@ -82,7 +82,7 @@ export const LoginModal: React.FC<LoginModalProps> = ({ open, onClose }) => {
       if (connector && !isDisconnected) {
         const connectedAccount = await waitForAccount()
         console.log("connected account ", connectedAccount)
-        setAddress(connectedAccount.address)
+        setAddress(connectedAccount?.address)
         setIsConnected(true)
       }
     })()
