6a9392076f19a45afcd457e1ffd6404bc0e46ff7
jadapema <jadapema@gmail.com>
2024-12-13 14:18:23 -0600

    fix: like counter

--- src/sections/publication/publication-comment-item.tsx
+++ src/sections/publication/publication-comment-item.tsx
@@ -96,13 +96,15 @@ export default function PublicationCommentItem({ comment, hasReply, canReply }: 
           comment_id: comment?.id,
           rawDescription: `${sessionData?.profile?.metadata?.displayName} liked your comment`,
         });
+
         if (!hasLiked){
           dispatch(incrementCounterLikes(comment.id));
+        } else {
+          dispatch(decrementCounterLikes(comment.id));
         }
+
         if(!hasLiked && comment?.by?.id !== sessionData?.profile?.id) {
           sendNotification(comment?.by?.id, sessionData?.profile?.id, notificationPayload);
-        }else {
-          dispatch(decrementCounterLikes(comment.id));
         }
       });
       setHasLiked(!hasLiked); // Toggle the UI based on the reaction state
