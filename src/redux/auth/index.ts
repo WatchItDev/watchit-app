8afbb1b35d926a32618a34a76c2ebe336a6812fa
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-12-17 19:47:48 -0600

    feat(auth): add currentStep state and relevant actions
    - Added `currentStep` to the auth slice state in `src/redux/auth/index.ts`.
    - Updated `toggleModalCreationProfile` to reset `currentStep` when the modal is closed.
    - Updated `setProfileCreationStep` to track the current step dynamically in `currentStep`.
    - Introduced `resetCurrentStep` action to explicitly reset the `currentStep` to 0.

--- src/redux/auth/index.ts
+++ src/redux/auth/index.ts
@@ -6,6 +6,7 @@ export type AuthReducerState = {
   isAuthLoading: boolean;
   balance: number;
   modalCreationProfile: boolean;
+  currentStep: number;
   profileCreationSteps: {
     step1: 'idle' | 'running' | 'finished';
     step2: 'idle' | 'running' | 'finished';
@@ -18,6 +19,7 @@ const initialState: AuthReducerState = {
   isAuthLoading: false,
   balance: 0,
   modalCreationProfile: false,
+  currentStep: 0,
   profileCreationSteps: {
     step1: 'idle',
     step2: 'idle',
@@ -43,21 +45,25 @@ const authSlice = createSlice({
     },
     toggleModalCreationProfile: (state) => {
       state.modalCreationProfile = !state.modalCreationProfile;
-      // IF the modal is closed, reset the steps
       if (!state.modalCreationProfile) {
         state.profileCreationSteps.step1 = 'idle';
         state.profileCreationSteps.step2 = 'idle';
         state.profileCreationSteps.step3 = 'idle';
+        state.currentStep = 0;
       }
     },
     setProfileCreationStep: (state, action: PayloadAction<{ step: number; status: 'idle' | 'running' | 'finished' }>) => {
       const { step, status } = action.payload;
       // @ts-ignore
       state.profileCreationSteps[`step${step}`] = status;
+      state.currentStep = step;
+    },
+    resetCurrentStep: (state) => {
+      state.currentStep = 0;
     },
   },
 });
 
-export const { openLoginModal, closeLoginModal, setAuthLoading, setBalance, toggleModalCreationProfile, setProfileCreationStep } = authSlice.actions;
+export const { openLoginModal, closeLoginModal, setAuthLoading, setBalance, toggleModalCreationProfile, setProfileCreationStep, resetCurrentStep } = authSlice.actions;
 
 export default authSlice.reducer;
