83548120884da2a384d3bef7e647a742cce68857
Geolffrey Mena <gmjun2000@gmail.com>
2025-01-20 21:20:43 -0600

    design: improved size of invite to refer

--- src/sections/user/profile-header.tsx
+++ src/sections/user/profile-header.tsx
@@ -62,7 +62,7 @@ const shareLinks = [
   {
     icon: 'mingcute:social-x-line',
     label: 'X',
-    url: `https://x.com/share/?url=${encodeURIComponent(urlToShare)}&text=Visit%20my%20profile%20in%20Watchit&hashtags=Watchit,Blockchain,Crypto`,
+    url: `https://x.com/share/?url=${encodeURIComponent(urlToShare)}&text=Visit%20my%20profile%20on%20Watchit&hashtags=Watchit,Blockchain,Crypto`,
   },
   {
     icon: 'mdi:facebook',
