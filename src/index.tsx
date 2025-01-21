9c8331977a036e3a87ac587dc47972fda8486d56
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-01-20 21:12:08 -0600

    fix: update typo in Sentry DSN and adjust minor details
    - **src/config-global.ts**: Correct typo from `SENTRY_DNS` to `SENTRY_DSN`.
    - **src/index.tsx**: Update Sentry configuration to reflect the corrected `dsn` key and add `tracePropagationTargets`.
    - **src/sections/user/profile-header.tsx**: Fix typo in share text, changing "in Watchit" to "on Watchit".
    
    These changes ensure proper functionality and correct phrasing across the application.

--- src/index.tsx
+++ src/index.tsx
@@ -19,10 +19,11 @@ const isDevelopment = GLOBAL_CONSTANTS.ENVIRONMENT === 'development';
 Sentry.init({
   environment: GLOBAL_CONSTANTS.ENVIRONMENT,
   enabled: !isDevelopment,
-  dsn: GLOBAL_CONSTANTS.SENTRY_DNS,
+  dsn: GLOBAL_CONSTANTS.SENTRY_DSN,
   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  tracePropagationTargets: ["localhost", /^https:\/\/app.watchit\.movie/],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
