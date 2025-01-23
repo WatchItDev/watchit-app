4e98039c4af438cc1f04fdef3c8244888367d811
Geolffrey Mena <gmjun2000@gmail.com>
2025-01-23 16:32:56 -0600

    chore: remove consoles

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 export default defineConfig(({ mode }) => {
   // Load environment variables based on the current mode
   const env = loadEnv(mode, process.cwd(), '');
-  const pure = mode === 'production' ? ['console.log', 'console.info'] : []
+  const pure = mode === 'production' ? ['console.log', 'console.info', 'console.warn'] : []
   
   return {
     esbuild: { pure },
