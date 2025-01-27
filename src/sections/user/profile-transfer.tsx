1e4dfb8365dd7aa35142aec2027aa78d4857179b
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-01-27 17:53:20 -0600

    feat: add profile transfer component
    - Introduced `ProfileTransfer` component in `src/sections/user/profile-transfer.tsx`.
    - Utilizes `FinanceQuickTransferModal` for transferring profiles with a maximum limit of 500.
    - Implements `useBoolean` hook to manage modal state efficiently.

---
+++ src/sections/user/profile-transfer.tsx
@@ -0,0 +1,41 @@
+import {Profile} from "@lens-protocol/api-bindings";
+import {FC} from "react";
+import FinanceQuickTransferModal from "@src/sections/finance/components/finance-quick-transfer-modal.tsx";
+import LoadingButton from "@mui/lab/LoadingButton";
+import {useBoolean} from "@src/hooks/use-boolean.ts";
+
+interface ProfileTransferProps {
+  profile: Profile;
+}
+
+const ProfileTransfer: FC<ProfileTransferProps> = ({profile}) => {
+  const confirm = useBoolean();
+
+  const handleOpen = () => {
+    confirm.onTrue();
+  }
+
+  const handleTransferFinish = () => {
+    confirm.onFalse();
+  }
+
+  return (
+    <>
+      <LoadingButton variant={'soft'} onClick={handleOpen}>
+        Transfer
+      </LoadingButton>
+
+      <FinanceQuickTransferModal
+        max={500}
+        amount={-1}
+        open={confirm.value}
+        address={profile?.ownedBy?.address}
+        onClose={confirm.onFalse}
+        onFinish={handleTransferFinish}
+        contactInfo={profile}
+      />
+    </>
+  )
+}
+
+export default ProfileTransfer;
