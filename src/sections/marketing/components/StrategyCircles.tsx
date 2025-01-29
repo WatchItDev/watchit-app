b6558a2316b8f70eb0470fb950eaee11cd70c36e
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-01-29 12:11:39 -0600

    feat: add responsiveness to StrategyCircles component
    - Updated `StrategyCircles` to use `useResponsive` for adapting styles based on screen size.
    - Adjusted sizes and border attributes for `MainCircle`, `InnerCircle`, and `CenterCircle` to vary based on the `lgUp` breakpoint.
    - Reduced default opacity of `CircleContainer` for consistency with design updates.

--- src/sections/marketing/components/StrategyCircles.tsx
+++ src/sections/marketing/components/StrategyCircles.tsx
@@ -1,16 +1,19 @@
 import { Box, styled } from '@mui/material';
+import {useResponsive} from "@src/hooks/use-responsive.ts";
 
 interface StrategyCirclesProps {
   color: string;
-  theme?: any;
+  lgUp?: boolean;
 }
 
 const StrategyCircles = ({color}: StrategyCirclesProps) => {
+  const lgUp = useResponsive('up','md');
+
   return (
     <CircleContainer>
-      <MainCircle color={color}>
-        <InnerCircle color={color}>
-          <CenterCircle color={color} />
+      <MainCircle color={color} lgUp={lgUp}>
+        <InnerCircle color={color}  lgUp={lgUp}>
+          <CenterCircle color={color}  lgUp={lgUp} />
         </InnerCircle>
       </MainCircle>
     </CircleContainer>
@@ -21,35 +24,35 @@ const CircleContainer = styled(Box)(() => ({
   position: 'absolute',
   bottom: -30,
   right: -35,
-  opacity: 0.5,
+  opacity: 0.2,
   zIndex: 1,
 }));
 
-const MainCircle = styled(Box)(({ color }: StrategyCirclesProps) => ({
-  width: '150px',
-  height: '150px',
+const MainCircle = styled(Box)(({ color, lgUp }: StrategyCirclesProps) => ({
+  width: lgUp ? '150px' : '100px',
+  height: lgUp ? '150px' : '100px',
   borderRadius: '50%',
-  border: `8px solid ${color}`,
+  border: `${lgUp ? 8 : 6}px solid ${color}`,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
 }));
 
-const InnerCircle = styled(Box)(({ color }: StrategyCirclesProps) => ({
-  width: '75px',
-  height: '75px',
+const InnerCircle = styled(Box)(({ color, lgUp }: StrategyCirclesProps) => ({
+  width: lgUp ? '75px' : '50px',
+  height: lgUp ? '75px' : '50px',
   borderRadius: '50%',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  border: `8px solid ${color}`,
+  border: `${lgUp ? 8 : 6}px solid ${color}`,
 }));
 
-const CenterCircle = styled(Box)(({ color }: StrategyCirclesProps) => ({
-  width: '25px',
-  height: '25px',
+const CenterCircle = styled(Box)(({ color, lgUp }: StrategyCirclesProps) => ({
+  width: lgUp ? '25px' : '15px',
+  height: lgUp ? '25px' : '15px',
   borderRadius: '50%',
-  border: `8px solid ${color}`,
+  border: `${lgUp ? 8 : 6}px solid ${color}`,
 }));
 
 export default StrategyCircles;
