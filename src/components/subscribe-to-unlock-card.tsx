c29773b9fed96c398609cc13c41e3bab1f034687
jadapema <jadapema@gmail.com>
2025-01-29 18:09:58 -0600

    feat: added ownership registration

--- src/components/subscribe-to-unlock-card.tsx
+++ src/components/subscribe-to-unlock-card.tsx
@@ -5,11 +5,11 @@ import { useGetPolicyTerms } from '@src/hooks/use-get-policy-terms.ts';
 import { Address } from 'viem';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { GLOBAL_CONSTANTS } from '@src/config-global.ts';
-import {Icon} from "@iconify/react";
-import NeonPaper from "@src/sections/publication/NeonPaperContainer.tsx";
-import {useState} from "react";
-import {COLORS} from "@src/layouts/config-layout.ts";
-import {styled} from "@mui/material/styles";
+// import {Icon} from "@iconify/react";
+// import NeonPaper from "@src/sections/publication/NeonPaperContainer.tsx";
+// import {useState} from "react";
+// import {COLORS} from "@src/layouts/config-layout.ts";
+// import {styled} from "@mui/material/styles";
 
 interface Props {
   post: any;
@@ -24,7 +24,7 @@ export const SubscribeToUnlockCard = ({
   subscribeDisabled,
   post,
 }: Props) => {
-  const [loadingTrial, setLoadingTrial] = useState(false);
+  // const [loadingTrial, setLoadingTrial] = useState(false);
 
   const { terms } = useGetPolicyTerms(
     GLOBAL_CONSTANTS.SUBSCRIPTION_POLICY_ADDRESS as Address,
@@ -34,15 +34,14 @@ export const SubscribeToUnlockCard = ({
   const totalCostWei = terms?.amount ? terms?.amount * BigInt(durationDays) : 0; // Calculate total cost in Wei: DAILY_COST_WEI * durationDays
   const totalCostMMC = ethers.formatUnits(totalCostWei, 18); // Converts Wei to MMC
 
+  // const handleTrial = () => {
+  //   setLoadingTrial(true);
+  //   setTimeout(() => {
+  //     setLoadingTrial(false);
+  //   }, 2000);
+  // };
 
-  const handleTrial = () => {
-    setLoadingTrial(true);
-    setTimeout(() => {
-      setLoadingTrial(false);
-    }, 2000);
-  };
-
-  const RainbowEffect = loadingTrial ? NeonPaper : Box;
+  // const RainbowEffect = loadingTrial ? NeonPaper : Box;
 
   return (
     <Card
@@ -86,18 +85,18 @@ export const SubscribeToUnlockCard = ({
           <IconPlayerPlay size={20} style={{ marginRight: 5 }} />
           Join
         </LoadingButton>
-        <RainbowEffect borderRadius={'10px'}
-                   animationSpeed={'3s'}
-                   padding={'2px'}
-                   width={'auto'}>
+        {/*<RainbowEffect borderRadius={'10px'}*/}
+        {/*           animationSpeed={'3s'}*/}
+        {/*           padding={'2px'}*/}
+        {/*           width={'auto'}>*/}
 
-          <StyledBoxGradient onClick={handleTrial} loading={loadingTrial}>
-            <Icon icon="ic:outline-try" width="18" height="18" />
-            <Typography variant="body2" sx={{ lineHeight: 1, fontWeight: '700', ml: 1 }}>
-              Free trial
-            </Typography>
-          </StyledBoxGradient>
-        </RainbowEffect>
+        {/*  <StyledBoxGradient onClick={handleTrial} loading={loadingTrial}>*/}
+        {/*    <Icon icon="ic:outline-try" width="18" height="18" />*/}
+        {/*    <Typography variant="body2" sx={{ lineHeight: 1, fontWeight: '700', ml: 1 }}>*/}
+        {/*      Free trial*/}
+        {/*    </Typography>*/}
+        {/*  </StyledBoxGradient>*/}
+        {/*</RainbowEffect>*/}
         </Box>
         <Box sx={{ mt: 3, borderRadius: 1 }}>
           <Typography variant="body2" color="textSecondary">
@@ -111,20 +110,20 @@ export const SubscribeToUnlockCard = ({
   );
 };
 
-const StyledBoxGradient = styled(LoadingButton)(() => ({
-  width: '100%',
-  background: `linear-gradient(90deg, rgba(254,255,118,1) 25%, rgba(212,190,58,1) 50%, rgba(189,119,255,0.5) 100%)`,
-  backgroundSize: '400%',
-  animation: 'gradientShift 10s infinite',
-  padding: '12px',
-  color: COLORS.GRAY_DARK,
-  borderRadius: '8px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  '@keyframes gradientShift': {
-    '0%': { backgroundPosition: '0% 50%' },
-    '50%': { backgroundPosition: '100% 50%' },
-    '100%': { backgroundPosition: '0% 50%' },
-  },
-}));
+// const StyledBoxGradient = styled(LoadingButton)(() => ({
+//   width: '100%',
+//   background: `linear-gradient(90deg, rgba(254,255,118,1) 25%, rgba(212,190,58,1) 50%, rgba(189,119,255,0.5) 100%)`,
+//   backgroundSize: '400%',
+//   animation: 'gradientShift 10s infinite',
+//   padding: '12px',
+//   color: COLORS.GRAY_DARK,
+//   borderRadius: '8px',
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   '@keyframes gradientShift': {
+//     '0%': { backgroundPosition: '0% 50%' },
+//     '50%': { backgroundPosition: '100% 50%' },
+//     '100%': { backgroundPosition: '0% 50%' },
+//   },
+// }));
