1189d3fd3c1ccc0f9f332b81c67c542bcbb74948
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-02-13 11:03:30 -0600

    refactor(table): add customizable emptyText prop to TableNoData
    - Updated `TableNoData` component in `table-no-data.tsx` to include a new `emptyText` prop with a default value of 'No data found'.
    - Passed `emptyText` to the `EmptyContent` component, replacing the hardcoded 'No data found', enabling better flexibility for displaying custom messages.

--- src/components/table/table-no-data.tsx
+++ src/components/table/table-no-data.tsx
@@ -11,16 +11,17 @@ type Props = {
   loading: boolean;
   notFound: boolean;
   sx?: SxProps<Theme>;
+  emptyText?: string;
 };
 
-export default function TableNoData({ loading, notFound, sx }: Props) {
+export default function TableNoData({ loading, notFound, sx, emptyText= 'No data found' }: Props) {
   return (
     <TableRow>
       {notFound ? (
         <TableCell colSpan={12} sx={{ p: 0, pt: 2 }}>
           <EmptyContent
             filled
-            title={loading ? 'Loading ...' : 'No data found'}
+            title={loading ? 'Loading ...' : emptyText}
             sx={{
               py: 10,
               ...sx,
