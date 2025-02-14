c54bf816e1b5b0c81f4a9f71b7f49c1aab75f583
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-02-14 12:03:31 -0600

    feat(utils): add filtering function for hidden profiles
    Introduced `filterHiddenProfiles` to filter out profiles containing specific hidden indicators (`###HIDDEN###`) in their `displayName`, `bio`, or `id` fields.
    
    - src/utils/profile.ts:
      - Added `filterHiddenProfiles` function.
      - Imported `Profile` type from `@lens-protocol/api-bindings`.

--- src/utils/profile.ts
+++ src/utils/profile.ts
@@ -1,5 +1,6 @@
 import { profile as profileBuilder, MetadataAttributeType } from '@lens-protocol/metadata';
 import { ProfileData } from '@src/auth/context/lens/types';
+import {Profile} from "@lens-protocol/api-bindings";
 
 const removeEmptyValues = (obj: any): any =>
   Object.fromEntries(Object.entries(obj).filter(([_, v]) => v !== '' && v !== null));
@@ -36,4 +37,25 @@ export const buildProfileMetadata = (
   const cleanedMetadata = removeEmptyValues(metadata);
 
   return profileBuilder(cleanedMetadata);
+};
+
+/**
+ * Filters an array of profiles to exclude any profiles with hidden indicators.
+ *
+ * This function removes profiles that have a "displayName" or "bio" property containing
+ * the substring '###HIDDEN###'. Any profiles containing this substring in either property are excluded
+ * from the returned array.
+ *
+ * @param {Profile[]} profiles - The array of profile objects to filter.
+ * @return {any[]} An array of filtered profiles excluding those marked as hidden.
+ */
+export const filterHiddenProfiles = (profiles?: Profile[]): Profile[] | null | undefined => {
+  // displayName, bio and lens id properties are checked for the hidden indicator
+  const patterns = ['###HIDDEN###'];
+
+  // Filter profiles that do not contain the hidden indicator in the "displayName" or "bio" properties using the patterns array
+  return profiles?.filter((profile: Profile) => !patterns.some((pattern) => profile?.metadata?.displayName?.includes(pattern) || profile?.metadata?.bio?.includes(pattern) || profile?.id.includes(pattern)));
+
+  // @ts-ignore
+  //return profiles.filter((profile) => !profile?.metadata?.displayName?.includes('###HIDDEN###') && !profile?.metadata?.bio.includes('###HIDDEN###'));
 };
