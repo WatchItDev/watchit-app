6ed543006c53430d34ecd6e6b635efd03a48e048
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-02-20 10:17:21 -0600

    test: add cleanup hook to improve test environment setup
    - Updated `setupTest.ts` to include `cleanup` from `@testing-library/react`.
    - Added `afterEach` hook to ensure test artifacts are cleaned after each test.
    - These changes enhance test reliability and maintain a clean DOM state.

--- setupTest.ts
+++ setupTest.ts
@@ -1,11 +1,16 @@
 import * as matchers from '@testing-library/jest-dom/matchers';
-import { expect, vi } from 'vitest';
+import {afterEach, expect, vi} from 'vitest';
+import {cleanup} from "@testing-library/react";
 
 expect.extend(matchers);
 
+// Test Hooks
+afterEach(() => {
+  cleanup();
+})
+
 // Mocks
 vi.mock('react-i18next');
-
 
 // @ts-ignore
 window.matchMedia = window.matchMedia || function() {
