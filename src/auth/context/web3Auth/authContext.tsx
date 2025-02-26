8d2787158b4e2954c307ab8ddbf293486467e062
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-02-26 16:40:39 -0600

    refactor(auth): update typing and imports in Web3Auth context
    - Updated `web3Auth` prop typing in `AuthContextProvider` to use `Web3Auth` from `@web3auth/modal` module.
    - Removed unnecessary `React` import and adjusted imports accordingly in `authContext.tsx`.

--- src/auth/context/web3Auth/authContext.tsx
+++ src/auth/context/web3Auth/authContext.tsx
@@ -1,8 +1,8 @@
-import React, {createContext} from "react";
+import {createContext} from "react";
 import {AuthContextProps} from "./types";
-
+import { Web3Auth } from "@web3auth/modal/dist/types/modalManager";
 export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
-export const AuthContextProvider: React.FC<{web3Auth: any; children: React.ReactNode}> = ({
+export const AuthContextProvider: React.FC<{web3Auth: Web3Auth; children: React.ReactNode}> = ({
   web3Auth,
   children,
 }) => {
