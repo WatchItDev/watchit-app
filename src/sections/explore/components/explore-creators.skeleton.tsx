0e9b83b97425d3d276a093ccff929ac351703054
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-03-10 16:30:55 -0600

    refactor: extract SkeletonItem component in skeleton files
    - In `explore-publications.skeleton.tsx`, extracted the Skeleton logic into a standalone `SkeletonItem` component for reuse and improved readability.
    - In `explore-creators.skeleton.tsx`, moved the inline skeleton logic into a separate `SkeletonItem` component, mirroring the pattern used in publications skeleton.
    - Updated both files to pass `SkeletonItem` as a prop to `ExploreCarouselSkeleton`.

--- src/sections/explore/components/explore-creators.skeleton.tsx
+++ src/sections/explore/components/explore-creators.skeleton.tsx
@@ -2,44 +2,50 @@ import React from 'react';
 import { Box, Skeleton } from '@mui/material';
 import { ExploreCarouselSkeleton } from '@src/sections/explore/components/explore-carousel.skeleton.tsx';
 
+
+
+const SkeletonItem = () => {
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        border: '1px solid rgba(255, 255, 255, 0.12)',
+        padding: 1,
+        borderRadius: '1rem',
+        mb: 1,
+      }}
+    >
+      <Skeleton
+        variant="rectangular"
+        sx={{
+          borderRadius: 1,
+          width: '100%',
+          height: '8rem',
+        }}
+      />
+      <Skeleton
+        variant="rectangular"
+        sx={{
+          position: 'absolute',
+          top: '6rem',
+          left: '1.5rem',
+          width: '3rem',
+          height: '3rem',
+          borderRadius: 1,
+        }}
+      />
+      <Skeleton variant="text" sx={{ width: '50%', mt: 2 }} />
+      <Skeleton variant="text" sx={{ width: '100%' }} />
+    </Box>
+  )
+}
+
 export const ExploreCreatorsSkeleton: React.FC = () => {
   return (
     <ExploreCarouselSkeleton
       title="Latest creators"
-      SkeletonItemComponent={() => (
-        <Box
-          sx={{
-            position: 'relative',
-            width: '100%',
-            border: '1px solid rgba(255, 255, 255, 0.12)',
-            padding: 1,
-            borderRadius: '1rem',
-            mb: 1,
-          }}
-        >
-          <Skeleton
-            variant="rectangular"
-            sx={{
-              borderRadius: 1,
-              width: '100%',
-              height: '8rem',
-            }}
-          />
-          <Skeleton
-            variant="rectangular"
-            sx={{
-              position: 'absolute',
-              top: '6rem',
-              left: '1.5rem',
-              width: '3rem',
-              height: '3rem',
-              borderRadius: 1,
-            }}
-          />
-          <Skeleton variant="text" sx={{ width: '50%', mt: 2 }} />
-          <Skeleton variant="text" sx={{ width: '100%' }} />
-        </Box>
-      )}
+      SkeletonItemComponent={() => <SkeletonItem />}
     />
   );
 };
