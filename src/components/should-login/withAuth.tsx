a8bff74e112c03f30ca2d4e8ea5803cd7570abd2
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2025-03-31 18:19:53 -0600

    refactor: update withAuth to use destructured props and types
    Refactored the withAuth HOC to use destructured props for improved readability and type safety.
    
    - Modified `withAuth` to accept a single destructured `WithAuthProps` object instead of individual parameters.
    - Updated `WithAuth` component to align with these changes and ensure correct type usage.
    - Minor formatting adjustments for consistent code style.

--- src/components/should-login/withAuth.tsx
+++ src/components/should-login/withAuth.tsx
@@ -1,11 +1,11 @@
-import React, {FC} from 'react';
-import ShouldLogin from '@src/components/should-login/should-login';
-import { LoadingScreen } from '@src/components/loading-screen';
-import { WithAuthProps } from '@src/components/should-login/types.ts';
-import { useAuth } from '@src/hooks/use-auth.ts';
+import { FC } from "react";
+import ShouldLogin from "@src/components/should-login/should-login";
+import { LoadingScreen } from "@src/components/loading-screen";
+import { WithAuthProps } from "@src/components/should-login/types.ts";
+import { useAuth } from "@src/hooks/use-auth.ts";
 
-const withAuth  = (WrappedComponent: React.ComponentType, header: string,  icon: string, description: string) => {
-  return (props: any) => {
+const withAuth = ({ component: WrappedComponent, header, icon, description }: WithAuthProps) => {
+  return () => {
     const { session: sessionData, isSessionLoading: loading } = useAuth();
 
     if (loading) {
@@ -16,11 +16,16 @@ const withAuth  = (WrappedComponent: React.ComponentType, header: string,  icon:
       return <ShouldLogin icon={icon} description={description} title={header} />;
     }
 
-    return <WrappedComponent {...props} />;
+    return <WrappedComponent />;
   };
 };
 
-export const WithAuth: FC<WithAuthProps> = ({ component: Component, header, icon, description}) => {
-  const AuthComponent = withAuth(Component, header, icon, description);
+export const WithAuth: FC<WithAuthProps> = ({
+  component: Component,
+  header,
+  icon,
+  description,
+}) => {
+  const AuthComponent = withAuth({ component: Component, header, icon, description });
   return <AuthComponent />;
 };
