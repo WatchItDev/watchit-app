c33d94d1958f8598a7dc131040c140d81b3cd485
soy eddy <soyeddy.email.com>
2025-04-14 19:16:02 -0600

    test(explore-loader): renders the LoadingScreen component with correct styles..

--- src/sections/explore/components/__test__/explore-loader.test.tsx
+++ src/sections/explore/components/__test__/explore-loader.test.tsx
@@ -3,7 +3,25 @@ import { render, screen } from "@testing-library/react";
 import ExploreLoader from "../explore-loader";
 
 vi.mock("@src/components/loading-screen", () => ({
-  LoadingScreen: () => <div data-testid="loading-screen">Loading...</div>,
+  LoadingScreen: () => (
+    <div
+      data-testid="loading-screen"
+      style={{
+        zIndex: "999",
+        background: "#1E1F22",
+        position: "absolute",
+        top: "0",
+        left: "0",
+        bottom: "0",
+        pointerEvents: "none",
+        right: "0",
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+      }}>
+      Loading...
+    </div>
+  ),
 }));
 
 describe("Testing in the <explore-loader/> component ", () => {
@@ -15,5 +33,23 @@ describe("Testing in the <explore-loader/> component ", () => {
   it("renders the LoadingScreen component within the layout", () => {
     render(<ExploreLoader />);
     expect(screen.getByTestId("loading-screen")).toBeInTheDocument();
+  });
+
+  it("renders the LoadingScreen component with correct styles", () => {
+    render(<ExploreLoader />);
+    const loadingScreen = screen.getByTestId("loading-screen");
+    expect(loadingScreen).toHaveStyle({
+      zIndex: "999",
+      background: "#1E1F22",
+      position: "absolute",
+      top: "0",
+      left: "0",
+      bottom: "0",
+      pointerEvents: "none",
+      right: "0",
+      display: "flex",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+    });
   });
 });
