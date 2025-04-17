a5fb3c6e21da0829b72d9fc0603cf634babb9784
soy eddy <soyeddy.email.com>
2025-04-17 12:18:14 -0600

    test(ExploreCarouselSkeleton): mock useItemsPerSlide and improve test for skeleton item count

--- src/sections/explore/components/__test__/explore-carousel.skeleton.test.tsx
+++ src/sections/explore/components/__test__/explore-carousel.skeleton.test.tsx
@@ -2,6 +2,13 @@ import { describe, expect, it } from "vitest";
 import { render } from "@testing-library/react";
 import { ExploreCarouselSkeleton } from "../explore-carousel.skeleton";
 
+
+vi.mock('@src/hooks/components/use-item-per-slide', () => ({
+  useItemsPerSlide: () => ({
+    itemsPerSlide: 3,
+  }),
+}));
+
 describe("Testing in the <ExploreCarouselSkeleton/> component", () => {
   it("should match snapshot", () => {
     const { container } = render(<ExploreCarouselSkeleton title="Test" />);
@@ -13,10 +20,9 @@ describe("Testing in the <ExploreCarouselSkeleton/> component", () => {
     expect(getByText("Test")).toBeInTheDocument();
   });
 
-  it("should render the correct number of skeleton items", () => {
+  it('should render 6 skeleton items if itemsPerSlide is 3', () => {
     const { getAllByTestId } = render(<ExploreCarouselSkeleton title="Test" />);
-    const skeletonItems = getAllByTestId("skeleton-item");
-
-    expect(skeletonItems.length).toBe(2);
+    const skeletonItems = getAllByTestId('skeleton-item');
+    expect(skeletonItems.length).toBe(6);
   });
 });
