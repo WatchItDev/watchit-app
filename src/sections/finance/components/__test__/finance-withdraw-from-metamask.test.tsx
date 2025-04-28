27e740c876bf84a574a36323c4ae894b8c45c733
soy eddy <soyeddy.email.com>
2025-04-28 10:20:28 -0600

    test(FinanceWithdrawModal): add initial test suite and snapshot for component rendering

--- src/sections/finance/components/__test__/finance-withdraw-from-metamask.test.tsx
+++ src/sections/finance/components/__test__/finance-withdraw-from-metamask.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render} from "@testing-library/react";
 import FinanceWithdrawFromMetamask from "../finance-withdraw-from-metamask";
 import { Provider } from "react-redux";
 import { store } from "@src/redux/store";
---
+++ src/sections/finance/components/__test__/finance-withdraw-modal.test.tsx
@@ -0,0 +1,59 @@
+import { describe, expect, it } from "vitest";
+import { render } from "@testing-library/react";
+import FinanceWithdrawModal from "../finance-withdraw-modal";
+import { Provider } from "react-redux";
+import { store } from "@src/redux/store";
+
+vi.mock("@src/hooks/use-auth.ts", () => ({
+  useAuth: vi.fn(() => ({
+    session: {
+      address: "0x1111111111111111111111111111111111111111",
+    },
+  })),
+}));
+
+const mockWithdraw = vi.fn();
+
+vi.mock("@src/workers/backgroundTaskWorker?worker", () => {
+  return {
+    default: class {
+      postMessage() {}
+      terminate() {}
+      addEventListener() {}
+      removeEventListener() {}
+    },
+  };
+});
+
+vi.mock("@src/hooks/protocol/use-withdraw.ts", () => ({
+  useWithdraw: () => ({
+    withdraw: mockWithdraw,
+    isLoading: false,
+    error: null,
+  }),
+}));
+
+vi.mock("@src/hooks/protocol/use-get-vault-balance.ts", () => ({
+  useGetVaultBalance: () => ({
+    data: 1000n,
+    isLoading: false,
+    error: null,
+  }),
+}));
+const mockOnClose = vi.fn();
+const mockOpen = true;
+const renderComponent = render(
+  <Provider store={store}>
+    <FinanceWithdrawModal onClose={mockOnClose} open={mockOpen} />
+  </Provider>,
+);
+
+describe("[COMPONENTS] <FinanceWithdrawModal/> ", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+  it("to match snapshot", () => {
+    const { container } = renderComponent;
+    expect(container).toMatchSnapshot();
+  });
+});
