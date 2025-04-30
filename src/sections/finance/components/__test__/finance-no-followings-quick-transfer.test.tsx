e69d54467fbcf93343074b90d307b5efba8583af
soy eddy <soyeddy.email.com>
2025-04-30 11:31:04 -0600

    test(FinanceNoFollowingsQuickTransfer): enhance tests to verify correct rendering of messages

--- src/sections/finance/components/__test__/finance-no-followings-quick-transfer.test.tsx
+++ src/sections/finance/components/__test__/finance-no-followings-quick-transfer.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import FinanceNoFollowingsQuickTransfer from "../finance-no-followings-quick-transfer";
 
 const renderComponent = () => {
@@ -10,5 +10,13 @@ describe("<COMPONENTS> FinanceNoFollowingsQuickTransfer ", () => {
   it("to match snapshot", () => {
     const { container } = renderComponent();
     expect(container).toMatchSnapshot();
+  });
+
+  it("should render the correct message when there are no followings", () => {
+    renderComponent();
+    expect(screen.getByText(/Here appear your followings/i)).toBeInTheDocument();
+    expect(screen.getByText(/Perform a search to start a transfer/i)).toBeInTheDocument();
+    const orElements = screen.getAllByText(/OR/i);
+    expect(orElements.length).toBeGreaterThan(0);
   });
 });
