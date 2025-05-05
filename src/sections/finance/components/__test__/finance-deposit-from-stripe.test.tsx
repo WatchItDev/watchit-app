1c4b808c7c0e3983195e74eaf97b0b577cab1685
soy eddy <soyeddy.email.com>
2025-05-05 13:58:47 -0600

    test(FinanceDepositFromStripe): Refactor tests to use renderComponent function and enhance readability

--- src/sections/finance/components/__test__/finance-deposit-from-stripe.test.tsx
+++ src/sections/finance/components/__test__/finance-deposit-from-stripe.test.tsx
@@ -1,10 +1,34 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import FinanceDepositFromStripe from "../finance-deposit-from-stripe";
 
+const renderComponent = () => {
+  return render(<FinanceDepositFromStripe />);
+};
+
 describe("[COMPONENTS] <FinanceDepositFromStripe /> ", () => {
   it("to match snapshot", () => {
-    const { container } = render(<FinanceDepositFromStripe />);
+    const { container } = renderComponent();
     expect(container).toMatchSnapshot();
+
+    screen.debug();
+  });
+
+  it("should render the component correctly", () => {
+    renderComponent();
+    expect(screen.getByText(/This feature is coming soon/i)).toBeInTheDocument();
+    expect(
+      screen.getByText("We’re working hard to bring this feature to life. Check back soon!"),
+    ).toBeInTheDocument();
+  });
+
+  it("should have the correct styles", () => {
+    renderComponent();
+    const boxElement = screen.getByText(/This feature is coming soon/i).closest("div");
+    expect(boxElement).toHaveStyle("display: flex");
+    expect(boxElement).toHaveStyle("flex-direction: column");
+    expect(boxElement).toHaveStyle("align-items: center");
+    expect(boxElement).toHaveStyle("gap: 8px");
+    expect(boxElement).toHaveStyle("text-align: center");
   });
 });
