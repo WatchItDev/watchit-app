a5efcb09432a9d148c491984d55eb05716e5f87c
soy eddy <soyeddy.email.com>
2025-05-08 12:54:41 -0600

    test(FinanceDepositModal): Add test for rendering Smart Account content on click

--- src/sections/finance/components/__test__/finance-deposit-modal.test.tsx
+++ src/sections/finance/components/__test__/finance-deposit-modal.test.tsx
@@ -90,5 +90,11 @@ describe("[COMPONENTS]: <FinanceDepositModal />", () => {
     expect(screen.getByText("Metamask Deposit")).toBeInTheDocument();
   });
 
- 
+  it("renders Smart Account content when clicked", () => {
+    renderComponent();
+    fireEvent.click(screen.getByText("Smart Account"));
+    expect(screen.getByText("Smart Account Deposit")).toBeInTheDocument();
+  });
+
+  
 });
