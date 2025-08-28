b03e14a85dc1289c15999ecba20d1f08b5977bba
Geolffrey Mena <gmjun2000@gmail.com>
2025-08-28 14:04:08 -0600

    fix: no fetching

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+]);
---
+++ rsbuild.config.ts
@@ -0,0 +1,22 @@
+import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig, loadEnv } from '@rsbuild/core';
+import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
+import { pluginImageCompress } from '@rsbuild/plugin-image-compress';
+
+const { publicVars } = loadEnv({ prefixes: ['VITE_'] });
+
+export default defineConfig({
+  html: { title: "Watchit", template: './index.html' },
+  plugins: [pluginReact(), pluginNodePolyfill(), pluginImageCompress()],
+  dev: {
+    lazyCompilation: true,
+  },
+  source: {
+    define: publicVars
+  },
+  performance: {
+    chunkSplit: {
+      strategy: 'split-by-experience',
+    }
+  }
+});
